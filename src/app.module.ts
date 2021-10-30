840ee7734230fd4224b39ab5d9f5369ab35d73ba
Luco97 <renatoluco@gmail.com>
2021-10-30 18:56:48 -0300

    [App-root] ADD se agregan los modulos nuevos a la raiz.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MangaModule } from './manga/manga.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    MangaModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService
+  ],
 })
-export class AppModule {}
+export class AppModule {
+
+}
