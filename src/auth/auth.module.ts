52d9878717daea88566315367880e6c9410ec6a6
Luco97 <renatoluco@gmail.com>
2021-11-06 17:20:19 -0300

    [ADD] Se conecta a DB usando metodo que no conozco

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { DbConfigService } from '@Shared/services/db-config.service';
+import { SharedModule } from './../shared/shared.module';
 import { AuthController } from './controller/auth.controller';
 import { AuthService } from './services/auth.service';
 
 @Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            imports: [SharedModule],
+            inject:[DbConfigService],
+            useFactory: async ( dbService: DbConfigService): Promise<TypeOrmModuleOptions> => {
+                return dbService.getTypeORMconfig();
+            }
+        })
+    ],
     providers:[
         AuthService,
     ],
