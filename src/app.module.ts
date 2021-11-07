79c632f70d290593103c3fd7967cb2a5a9456e59
Luco97 <renatoluco@gmail.com>
2021-11-07 18:20:32 -0300

    [ADD] se agrega configuracion para utilizar variables de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MangaModule } from './manga/manga.module';
+// import { UserModule } from './db/user/user.module';
+// import { MangaEntitiesModule } from './db/manga-entities/manga-entities.module';
 
 @Module({
   imports: [
     AuthModule,
-    MangaModule
+    MangaModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    // UserModule
   ],
   controllers: [AppController],
   providers: [
@@ -15,5 +22,5 @@ import { MangaModule } from './manga/manga.module';
   ],
 })
 export class AppModule {
-
+  
 }
