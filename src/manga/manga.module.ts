f8049c47f9e14fcbcf64e680dd0a95fff5a47a46
Luco97 <renatoluco@gmail.com>
2021-11-07 19:25:13 -0300

    [ADD] se agrega entidad de manga para el modulo de manga

--- src/manga/manga.module.ts
+++ src/manga/manga.module.ts
@@ -3,10 +3,22 @@ import { MangaController } from './controller/manga.controller';
 import { AuthGuard } from './guards/auth.guard';
 import { LoginMiddleware } from './middleware/login.middleware';
 import { SharedModule } from '../shared/shared.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { DbConfigService } from '@Shared/services/db-config.service';
+import { MangaEntitiesModule } from '../db/manga-entities/manga-entities.module';
 
 @Module({
   imports: [
-    SharedModule
+    SharedModule,
+    TypeOrmModule.forRootAsync({
+      imports: [SharedModule],
+      inject: [DbConfigService],
+      useFactory: async ( dbService: DbConfigService): Promise<TypeOrmModuleOptions> => {
+        console.log('Conexion desde manga...');
+        return await dbService.getTypeORMconfig();
+      }
+    }),
+    MangaEntitiesModule
   ],
   controllers: [
     MangaController
