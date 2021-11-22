303f339660bdfd914dcb8f0215155cebbf308308
Luco97 <renatoluco@gmail.com>
2021-11-22 19:05:50 -0300

    [ADD] se agregan las entidades para sincronizar y manipular con typeorm

--- src/db/manga-entities/manga-entities.module.ts
+++ src/db/manga-entities/manga-entities.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
-import { MangaService } from './service/manga.service';
+import { MangaService } from './services/manga.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MangaEntity } from './entity/manga.entity';
+import { ArtistService } from './services/artist.service';
+import { LanguageService } from './services/language.service';
+import { GenreService } from './services/genre.service';
+import { ArtistEntity } from './entity/artist.entity';
+import { LanguageEntity } from './entity/language.entity';
+import { GenreEntity } from './entity/genre.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([MangaEntity])
+    TypeOrmModule.forFeature([MangaEntity, ArtistEntity, LanguageEntity, GenreEntity])
   ],
-  providers: [MangaService]
+  providers: [MangaService, ArtistService, LanguageService, GenreService,]
 })
 export class MangaEntitiesModule {}
