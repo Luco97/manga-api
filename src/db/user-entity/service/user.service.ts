6cdc11cba9b285f31e378a8767511c88bee925f0
Luco97 <renatoluco@gmail.com>
2021-11-27 17:38:09 -0300

    [ADD] se crea el servicio basico para la tabla usuario

--- src/db/user-entity/service/user.service.ts
+++ src/db/user-entity/service/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -9,8 +9,23 @@ import { UserEntity } from '../entity/user.entity';
 export class UserService {
 
     constructor(
-        @InjectRepository(UserEntity) private userRepository: Repository<UserEntity>
+        @InjectRepository(UserEntity)
+        private userRepository: Repository<UserEntity>
     ) {}
 
+    async findAll(): Promise<UserEntity[]> {
+        const data = await this.userRepository.find();
+        return data;
+    }
+
+    async findOne(id: number): Promise<UserEntity> {
+        const data: UserEntity = await this.userRepository.findOne(id);
+        if(!data) throw new NotFoundException('No se puede encontrar usuario');
+        return data;
+    }
+
+    async create( user: UserEntity) {
+        return await this.userRepository.save({...user} as UserEntity);
+    }
 
 }
