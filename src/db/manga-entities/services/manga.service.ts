5718178d2e51a4f6357a5190fb29e8383155c7af
Luco97 <renatoluco@gmail.com>
2021-11-29 20:17:34 -0300

    [CHG] se usa la propiedad de opciones de typeorm para obtener las tablas relacionales

--- src/db/manga-entities/services/manga.service.ts
+++ src/db/manga-entities/services/manga.service.ts
@@ -15,70 +15,70 @@ import { LanguageEntity } from '../entity/language.entity'; */
 import { UserEntity } from '@userDB/entity';
 /* import { UserEntity } from '../../user-entity/entity/user.entity'; */
 
-
 @Injectable()
 export class MangaService {
-    constructor(
-        @InjectRepository(MangaEntity) 
-        private mangaRepository: Repository<MangaEntity>
-    ) {}
+  constructor(
+    @InjectRepository(MangaEntity)
+    private mangaRepository: Repository<MangaEntity>,
+  ) {}
 
-    async findAll(): Promise<MangaEntity[]> {
-        return await this.mangaRepository.find();
+  /**
+   * Encuentra todos los mangas en BD.
+   * @param {string[]} relations - Vector con relaciones, [ ] no toma ninguna tabla relacional.
+   */
+  async findAll(relations: string[] = ['users', 'genres', 'languages', 'artists']): Promise<MangaEntity[]> {
+    return await this.mangaRepository.find({ relations });
+  }
+
+  async findOne(id: number, relations: string[] = ['users', 'genres', 'languages', 'artists']): Promise<MangaEntity> {
+    const data: MangaEntity = await this.mangaRepository.findOne(id, {relations});
+    if (!data) throw new NotFoundException(`No existe el manga con id=${id}`);
+    return data;
+  }
+
+  async create(manga: MangaEntity) {
+    return await this.mangaRepository.save({ ...manga } as MangaEntity);
+  }
+
+  async delete(manga: MangaEntity) {
+    const data = await this.findOne(manga.id,[]);
+    return await this.mangaRepository.remove(data);
+  }
+
+  async addFavorite(manga: MangaEntity, user: UserEntity) {
+    const comic: MangaEntity = await this.findOne(manga.id, ['users']);
+    /* const comic: MangaEntity = await this.mangaRepository.findOne(manga.id, {}); */
+    comic.users.push({ ...user } as UserEntity);
+    return await this.mangaRepository.save(comic);
+  }
+
+  async subtractFavorite(manga: MangaEntity, user: UserEntity) {
+    const comic: MangaEntity = await this.findOne(manga.id, ['users']);
+    const value: number = comic.users.indexOf(user);
+    if (value >= 0) {
+      comic.users.splice(value, 1);
+      return await this.mangaRepository.save(comic);
     }
+    return new NotFoundException(
+      `El usuario con id=${user.id} y username='${user.username}' no existe en los favoritos del manga con id=${manga.id} y titulo='${manga.title}'`,
+    );
+  }
 
-    /* async findOne(id: number): Promise<MangaEntity> {
-        
-        const data = await this.mangaRepository.findOne(id)
-        return data;
-    } */
+  async addCategory(manga: MangaEntity, genre: GenreEntity) {
+    const comic = await this.findOne(manga.id, ['genres']);
+    comic.genres.push({ ...genre } as GenreEntity);
+    return await this.mangaRepository.save(comic);
+  }
 
-    async findOne( id: number): Promise<MangaEntity> {
-        const data: MangaEntity = await this.mangaRepository.findOne(id);
-        if(!data) throw new NotFoundException('No existe el manga');
-        return data;
-    }
+  async addWriter(manga: MangaEntity, artist: ArtistEntity) {
+    const comic: MangaEntity = await this.findOne(manga.id, ['artists']);
+    comic.artists.push({ ...artist } as ArtistEntity);
+    return await this.mangaRepository.save(comic);
+  }
 
-    async create( manga: MangaEntity) {
-        return await this.mangaRepository.save( {...manga} as MangaEntity);
-    }
-
-    async delete( manga: MangaEntity) {
-        const data = await this.findOne( manga.id);
-        return await this.mangaRepository.remove(data);
-    }
-
-    async addFavorite( manga: MangaEntity, user: UserEntity) {
-        const comic: MangaEntity = await this.findOne(manga.id);
-        comic.users.push( {...user} as UserEntity);
-        return await this.mangaRepository.save(comic);
-    }
-
-    async subtract( manga: MangaEntity, user: UserEntity) {
-        const comic: MangaEntity = await this.findOne(manga.id);
-        const value: number = comic.users.indexOf(user);
-        if(value >= 0) {
-            comic.users.splice(value,1);
-            return await this.mangaRepository.save(comic);
-        }
-        return new NotFoundException(`El usuario con id=${user.id} y username='${user.username}' no existe en los favoritos del manga con id=${manga.id} y titulo='${manga.title}'`)
-    }
-
-    async addCategory( manga: MangaEntity, genre: GenreEntity) {
-        const comic = await this.findOne(manga.id);
-        comic.genres.push({...genre} as GenreEntity);
-        return await this.mangaRepository.save(comic);
-    }
-
-    async addWriter( manga: MangaEntity, artist: ArtistEntity) {
-        const comic: MangaEntity = await this.findOne(manga.id);
-        comic.artists.push( {...artist} as ArtistEntity);
-        return await this.mangaRepository.save(comic);
-    }
-
-    async addTranslate( manga: MangaEntity, language: LanguageEntity) {
-        const comic: MangaEntity = await this.findOne(manga.id);
-        comic.languages.push( {...language} as LanguageEntity);
-        return await this.mangaRepository.save(comic);
-    }
+  async addTranslate(manga: MangaEntity, language: LanguageEntity) {
+    const comic: MangaEntity = await this.findOne(manga.id, ['languages']);
+    comic.languages.push({ ...language } as LanguageEntity);
+    return await this.mangaRepository.save(comic);
+  }
 }
