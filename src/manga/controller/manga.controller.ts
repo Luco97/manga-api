dfa66d45b151f30bcccf09fe0ee295c79b0ec213
Luco97 <renatoluco@gmail.com>
2021-12-03 22:29:34 -0300

    [ADD] se agregan los controladores para definir sus peticiones (por entidad en posibles casos)

--- src/manga/controller/manga.controller.ts
+++ src/manga/controller/manga.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Get, Res, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('manga')
-export class MangaController {}
+@UseGuards(AuthGuard)
+export class MangaController {
+    constructor( ) {}
+    /* @Get('x')
+    getManga(@Res() res: Response) {
+        res.status(HttpStatus.OK)
+            .json({
+                message: 'Everithing works fine !!!'
+            })
+    } */
+}
