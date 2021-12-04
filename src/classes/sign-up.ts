84eb4c0c2c791ea20055fb73b4d3850b3b2a0498
Luco97 <renatoluco@gmail.com>
2021-12-03 22:26:48 -0300

    [CHG] se agregan distintos parametros en validaciones para utilizar las distintas opciones y ventajas en uso de class-validator

--- src/classes/sign-up.ts
+++ src/classes/sign-up.ts
@@ -1,14 +1,35 @@
-import { IsEmail, IsNotEmpty, Length } from "class-validator";
+import { IsEmail, IsNotEmpty, Length, ValidationArguments } from "class-validator";
 
 export class SignUp {
+    @IsNotEmpty({
+        message: 'email  no ingresado'
+    })
     @IsEmail()
+    @Length(8,30, {
+        message: typeNumber(4, 30, 'email')
+    })
     email;
 
-    @IsNotEmpty()
-    @Length( 5, 18)
+    @IsNotEmpty({
+        message: 'username no ingresado'
+    })
+    @Length( 5, 18, {
+        message: typeNumber( 5, 18, 'username')
+    })
     username;
 
-    @IsNotEmpty()
-    @Length( 6, 20)
+    @IsNotEmpty({
+        message: 'password no ingresada'
+    })
+    @Length( 6, 20, {
+        message: typeNumber( 6, 20, 'password')
+    })
     password;
+}
+
+function typeNumber(min: number, max: number, name: string) {
+    return (validationArgument: ValidationArguments): string => {
+        if(validationArgument.value < min)  return `${name} bajo el minimo de caracteres`;
+        if(validationArgument.value > max) return  `${name} supera el maximo de caracteres`;
+    }
 }
