23482ec585c3ef7489681bf6c2f953a24d69e59a
Luco97 <renatoluco@gmail.com>
2021-12-07 22:09:24 -0300

    [ADD] se agrega createDTO para language

--- src/manga/classes/language.dto.ts
+++ src/manga/classes/language.dto.ts
@@ -1,1 +1,11 @@
-export class LanguageDto {}
+import { IsNotEmpty, Length } from "class-validator";
+
+export class createLanguageDto {
+    @IsNotEmpty({
+        message: `Falta definir la propiedad 'language'`
+    })
+    @Length( 1, 30, {
+        message: 'Esta bajo el minimo/Supera el maximo de caracteres'
+    })
+    language: string;
+}
