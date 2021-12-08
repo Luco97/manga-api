5a311dfadbdcb3b46f8ec72714a3f0fe0369f9cd
Luco97 <renatoluco@gmail.com>
2021-12-07 22:09:14 -0300

    [ADD] se agrega createDTO para genero

--- src/manga/classes/genre.dto.ts
+++ src/manga/classes/genre.dto.ts
@@ -1,1 +1,11 @@
-export class GenreDto {}
+import { IsNotEmpty, Length } from "class-validator";
+
+export class createGenreDto {
+    @IsNotEmpty({
+        message: `Falta la propiedad 'tag'`
+    })
+    @Length( 1, 30, {
+        message: 'Esta bajo el minimo/Supera el maximo de caracteres'
+    })
+    tag: string;
+}
