180086d353fc8c53e582cca5a200cec130a19848
Luco97 <renatoluco@gmail.com>
2021-12-19 19:26:08 -0300

    [ADD] se agregan condiciones de login cuando un usuario ya se encuentra activo y se crea metodo para logout

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -52,11 +52,58 @@ export class AuthService {
         user.password,
       );
       if (passCompare) {
+          if(user.active) {
+            return {
+              status: HttpStatus.CONFLICT,
+              message: 'Datos invalidos (El usuario se encuentra actualmente en uso)'
+            }
+          }
           const token: string = this._jwtService.generateJWT( user.id, user.username);
+          user.password = loginUser.password;
+          this._userService.updateStatus(user);
         return {
           status: HttpStatus.OK,
           message: `Bienvenido ${user.username}`,
           token
+        };
+      }
+      return {
+        status: HttpStatus.NOT_FOUND,
+        message: 'Datos invalidos',
+      };
+    }
+    return {
+      status: HttpStatus.NOT_FOUND,
+      message: 'Datos invalidos (El usuario no existe)',
+    };
+  }
+
+  async logoutUser(loginUser: loginUserDto): Promise<response> {
+    const users: UserEntity[] = await this._userService.findBy({
+      where: {
+        email: loginUser.email,
+      },
+    });
+
+    const user: UserEntity = users.pop();
+
+    if (user) {
+      const passCompare: boolean = await compare(
+        loginUser.password,
+        user.password,
+      );
+      if (passCompare) {
+          if(user.active) {
+            user.password = loginUser.password;
+            this._userService.updateStatus(user);
+            return {
+              status: HttpStatus.OK,
+              message: 'Sesion cerrada con exito' 
+            }
+          }
+        return {
+          status: HttpStatus.CONFLICT,
+          message: `Datos invalidos (El usuario se encuentra desconectado)`
         };
       }
       return {
