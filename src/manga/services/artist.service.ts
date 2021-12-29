0d1d028f9f212b493571a6ca507329ef00385b45
Luco97 <renatoluco@gmail.com>
2021-12-28 23:29:16 -0300

    [ADD] se crea metodo para obtener todos los artistas que existen

--- src/manga/services/artist.service.ts
+++ src/manga/services/artist.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpStatus } from '@nestjs/common';
+import { ArtistService as artistService } from '@db/manga/services';
+import { response, Artist } from '@interface/mangaResponses.interface';
+import { ArtistEntity } from '@db/manga/entity';
 
 @Injectable()
-export class ArtistService {}
+export class ArtistService {
+
+    constructor(
+        private _artistService: artistService
+    ) {}
+    
+    async getAll(): Promise<{response: response, data?: Artist[]}> {
+        const data: ArtistEntity[] = await this._artistService.findAll([])
+        if(data.length) {
+            return {
+                response:{
+                    status: HttpStatus.OK,
+                    message: 'getAll artistas'
+                },
+                data: data
+            }
+        }
+        return {
+            response: {
+                status: HttpStatus.NO_CONTENT,
+                message: 'Sin artistas'
+            }
+        }
+    }
+    
+}
