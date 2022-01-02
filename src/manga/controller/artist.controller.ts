d9ad7640477cb4d92952331b017681964b18b809
Luco97 <renatoluco@gmail.com>
2022-01-02 20:18:19 -0300

    [ADD] se crea endpoint para obtener un artista

--- src/manga/controller/artist.controller.ts
+++ src/manga/controller/artist.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res, HttpStatus, UseGuards } from '@nestjs/common';
+import { Controller, Get, Res, HttpStatus, UseGuards, Param, ParseIntPipe } from '@nestjs/common';
 import { Response } from 'express';
 import { Artist, response } from '@interface/mangaResponses.interface';
 import { ArtistService } from '@manga/services';
@@ -18,6 +18,26 @@ export class ArtistController {
     ) {
         try {
             const foo: {response: response, data?: Artist[]} = await this._artistService.getAll();
+            return res.status(foo.response.status)
+                        .json(foo);
+        } catch (error) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .json({
+                            response:{
+                                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                                message: 'Error en el servidor'
+                            }
+                        })
+        }
+    }
+
+    @Get(':id')
+    async findOne(
+        @Param('id', ParseIntPipe) id: number, 
+        @Res() res: Response<{response: response, data?: Artist}>
+    ) {
+        try {
+            const foo: {response: response, data?: Artist} = await this._artistService.getOne(id, []);
             return res.status(foo.response.status)
                         .json(foo);
         } catch (error) {
