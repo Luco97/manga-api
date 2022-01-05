6b50673330771520c94cfd878d5c5231d2d62866
Luco97 <renatoluco@gmail.com>
2022-01-04 22:08:28 -0300

    [ADD] se crea endpoint para crear artista

--- src/manga/controller/artist.controller.ts
+++ src/manga/controller/artist.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Res, HttpStatus, UseGuards, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Res, HttpStatus, UseGuards, Param, ParseIntPipe, Post, Body } from '@nestjs/common';
 import { Response } from 'express';
 import { Artist, response } from '@interface/mangaResponses.interface';
 import { ArtistService } from '@manga/services';
 import { AuthGuard } from '../guards/auth.guard';
+import { createArtistDto } from '@manga/dto';
 
 @Controller('artist')
 @UseGuards(AuthGuard)
@@ -44,6 +45,26 @@ export class ArtistController {
             return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
                         .json({
                             response:{
+                                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                                message: 'Error en el servidor'
+                            }
+                        })
+        }
+    }
+
+    @Post()
+    async create(
+        @Body() createArtist: createArtistDto,
+        @Res() res: Response<{response: response, data?: Artist}>
+    ) {
+        try {
+            const foo: {response: response, data?: Artist} = await this._artistService.create(createArtist);
+            return res.status(foo.response.status)
+                        .json(foo);
+        } catch (error) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .json({
+                            response: {
                                 status: HttpStatus.INTERNAL_SERVER_ERROR,
                                 message: 'Error en el servidor'
                             }
