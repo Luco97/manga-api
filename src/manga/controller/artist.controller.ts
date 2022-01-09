67d15ac9926c4b8155b9b7e5c94e81c262ffe9cc
Luco97 <renatoluco@gmail.com>
2022-01-08 22:10:12 -0300

    [ADD] se crea endpoint para actualizar artista

--- src/manga/controller/artist.controller.ts
+++ src/manga/controller/artist.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Res, HttpStatus, UseGuards, Param, ParseIntPipe, Post, Body } from '@nestjs/common';
+import { Controller, Get, Res, HttpStatus, UseGuards, Param, ParseIntPipe, Post, Body, Put } from '@nestjs/common';
 import { Response } from 'express';
 import { Artist, response } from '@interface/mangaResponses.interface';
 import { ArtistService } from '@manga/services';
 import { AuthGuard } from '../guards/auth.guard';
-import { createArtistDto } from '@manga/dto';
+import { createArtistDto, updateArtistsDto } from '@manga/dto';
 
 @Controller('artist')
 @UseGuards(AuthGuard)
@@ -61,6 +61,26 @@ export class ArtistController {
             const foo: {response: response, data?: Artist} = await this._artistService.create(createArtist);
             return res.status(foo.response.status)
                         .json(foo);
+        } catch (error) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .json({
+                            response: {
+                                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                                message: 'Error en el servidor'
+                            }
+                        })
+        }
+    }
+
+    @Put(':id')
+    async update(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() updateArtist: updateArtistsDto,
+        @Res() res: Response<{ response: response, data?: Artist }>
+    ) {
+        try {
+            const foo: {response: response, data?: Artist} = await this._artistService.update(id, updateArtist);
+            return res.status(foo.response.status).json(foo);
         } catch (error) {
             return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
                         .json({
