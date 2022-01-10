1553ecabc13469d9b19c9e12e56c4a15991c9cfb
Luco97 <renatoluco@gmail.com>
2022-01-09 22:00:15 -0300

    [ADD] se crea endpoint para obtener todos los artistas

--- src/manga/controller/genre.controller.ts
+++ src/manga/controller/genre.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Genre, response } from '@interface/mangaResponses.interface';
+import { GenreService } from '@manga/services';
+import { Controller, Get, Res, UseGuards, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('genre')
-export class GenreController {}
+@UseGuards(AuthGuard)
+export class GenreController {
+
+    constructor(
+        private _genreService: GenreService
+    ) {}
+
+    @Get()
+    async getAll(
+        @Res() res: Response<{response: response, data?: Genre[]}>
+    ) {
+        try {
+            const foo: {response: response, data?: Genre[]} = await this._genreService.getAll();
+            return res.status(foo.response.status).json(foo);
+        } catch (error) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .json({
+                            response: {
+                                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                                message: 'Error en el servidor'
+                            }
+                        })
+        }
+    }
+}
