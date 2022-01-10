c3d2b01caba605cc50a557ca63c535a3238e63a8
Luco97 <renatoluco@gmail.com>
2022-01-10 20:50:47 -0300

    [ADD] se crea endpoint para obtener un genero

--- src/manga/controller/genre.controller.ts
+++ src/manga/controller/genre.controller.ts
@@ -1,6 +1,6 @@
 import { Genre, response } from '@interface/mangaResponses.interface';
 import { GenreService } from '@manga/services';
-import { Controller, Get, Res, UseGuards, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Res, UseGuards, HttpStatus, Param, ParseIntPipe } from '@nestjs/common';
 import { Response } from 'express';
 import { AuthGuard } from '../guards/auth.guard';
 
@@ -27,6 +27,19 @@ export class GenreController {
                                 message: 'Error en el servidor'
                             }
                         })
+        }
+    }
+
+    @Get(':id')
+    async getOne(
+        @Param('id', ParseIntPipe) id: number,
+        @Res() res: Response<{response: response, data?: Genre}>
+    ) {
+        try {
+            const foo: {response: response, data?: Genre} = await this._genreService.getOne(id, []);
+            return res.status(foo.response.status).json(foo);
+        } catch (error) {
+            
         }
     }
 }
