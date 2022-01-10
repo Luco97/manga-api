9e895cf8d2e4c7d7ee76f45a9ec1572a1bc9c763
Luco97 <renatoluco@gmail.com>
2022-01-09 21:58:16 -0300

    [ADD] se crea metodo getAll en servicio de genero

--- src/manga/services/genre.service.ts
+++ src/manga/services/genre.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpStatus } from '@nestjs/common';
+import { GenreService as genreService } from '@db/manga/services';
+import { Genre, response } from '@interface/mangaResponses.interface';
+import { GenreEntity } from '@db/manga/entity';
 
 @Injectable()
-export class GenreService {}
+export class GenreService {
+
+    constructor(
+        private _genreService: genreService
+    ) {}
+
+    async getAll(): Promise<{response: response, data?: GenreEntity[]}> {
+        const data: GenreEntity[] = await this._genreService.findAll([]);
+        if(data.length) {
+            return {
+                response:{
+                    status: HttpStatus.OK,
+                    message: 'getAll generos'
+                },
+                data
+            }
+        }
+        return {
+            response: {
+                status: HttpStatus.NO_CONTENT,
+                message: 'Sin generos'
+            }
+        }
+    }
+}
