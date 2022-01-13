b59ac6e01a1b1aed4fb83e8ed25f6e6c1948ef7a
Luco97 <renatoluco@gmail.com>
2022-01-12 22:59:35 -0300

    [ADD] se crean varaibles const para obtener las relaciones y se crea path en tsconfig

---
+++ src/db/manga-entities/const/relations.ts
@@ -0,0 +1,7 @@
+export const artistRelations: string[] = ['mangas'];
+
+export const genreRelations: string[] = ['mangas'];
+
+export const languageRelations: string[] = ['mangas'];
+
+export const mangaRelations: string[] = ['mangas'];
