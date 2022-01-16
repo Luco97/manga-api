ce106eb60b994468168a0e093c5803818e63b52d
Luco97 <renatoluco@gmail.com>
2022-01-15 23:16:03 -0300

    [ADD] se crea endpoint para obtener todos los idiomas

--- src/manga/controller/language.controller.ts
+++ src/manga/controller/language.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Language, response } from '@interface/mangaResponses.interface';
+import { LanguageService } from '@manga/services';
+import { Controller, Get, HttpStatus, Post, Res, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
+import { AuthGuard } from '../guards/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('language')
-export class LanguageController {}
+export class LanguageController {
+
+    constructor(
+        private _languageService: LanguageService
+    ) {}
+
+    @Get()
+    async getAll(
+        @Res() res: Response<{response: response, data?: Language[]}>
+    ) {
+        try {
+            const foo: {response: response, data?: Language[]} = await this._languageService.getAll();
+            return res.status(foo.response.status).json(foo);
+        } catch (error) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .json({
+                            response: {
+                                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                                message: 'Error en el servidor'
+                            }
+                        })
+        }
+    }
+
+    @Post(':id')
+    async getOne() {}
+
+    @Post()
+    async create() {}
+}
+
