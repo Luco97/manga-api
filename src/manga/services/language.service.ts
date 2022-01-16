54ae548aef47652e4362003cb47da16137b91d61
Luco97 <renatoluco@gmail.com>
2022-01-15 23:15:25 -0300

    [ADD] se crea metodo en servicio de idioma para obtener todos los idiomas

--- src/manga/services/language.service.ts
+++ src/manga/services/language.service.ts
@@ -1,5 +1,7 @@
+import { LanguageEntity } from '@db/manga/entity';
 import { LanguageEntityService } from '@db/manga/services';
-import { Injectable } from '@nestjs/common';
+import { Language, response } from '@interface/mangaResponses.interface';
+import { Injectable, HttpStatus } from '@nestjs/common';
 
 @Injectable()
 export class LanguageService {
@@ -7,4 +9,23 @@ export class LanguageService {
     constructor(
         private _languageService: LanguageEntityService
     ) {}
+
+    async getAll(): Promise<{response: response, data?: Language[]}> {
+        const data: LanguageEntity[] = await this._languageService.findAll([]);
+        if(data.length) {
+            return {
+                response: {
+                    status: HttpStatus.OK,
+                    message: 'getAll idiomas'
+                },
+                data
+            }
+        }
+        return {
+            response: {
+                status: HttpStatus.NO_CONTENT,
+                message: 'Sin idiomas'
+            }
+        }
+    }
 }
