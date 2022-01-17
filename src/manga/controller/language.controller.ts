0b82c5da7b758a0d3259cca126dfab87434512e4
Luco97 <renatoluco@gmail.com>
2022-01-16 23:43:08 -0300

    [ADD] se crea endpoint para obtener un idioma en base un id

--- src/manga/controller/language.controller.ts
+++ src/manga/controller/language.controller.ts
@@ -1,6 +1,7 @@
 import { Language, response } from '@interface/mangaResponses.interface';
+import { readLanguageDto } from '@manga/dto';
 import { LanguageService } from '@manga/services';
-import { Controller, Get, HttpStatus, Post, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, Post, Res, UseGuards, ParseIntPipe, Body } from '@nestjs/common';
 import { Response } from 'express';
 import { AuthGuard } from '../guards/auth.guard';
 
@@ -31,7 +32,24 @@ export class LanguageController {
     }
 
     @Post(':id')
-    async getOne() {}
+    async getOne(
+        @Param('id',ParseIntPipe) id: number,
+        @Body() readLanguage: readLanguageDto,
+        @Res() res: Response<{response: response, data?: Language}>
+    ) {
+        try {
+            const foo: {response: response, data?: Language} = await this._languageService.getOne(id, readLanguage.relations);
+            return res.status(foo.response.status).json(foo);
+        } catch (error) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .json({
+                            response: {
+                                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                                message: 'Error en el servidor'
+                            }
+                        })
+        }
+    }
 
     @Post()
     async create() {}
