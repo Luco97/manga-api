612196022c8498400f0ee5e56fa0faadb08cfc33
Luco97 <renatoluco@gmail.com>
2022-01-19 23:31:07 -0300

    [ADD] se crea decorador para validar si existen elementos dentro de un arreglo definido (Para traer datos con los o algunos campos)

---
+++ src/manga/classes/custom-validator/ArrayContainsSome.class-validator.ts
@@ -0,0 +1,22 @@
+import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator';
+
+export function ArrayContainsSome(property: string[], validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'ArrayContainsSome',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value: string[], args: ValidationArguments) {
+            for (let index = 0; index < value.length; index++) {
+                const element = value[index];
+                if(!property.includes(element)) 
+                    return false;
+            }
+            return true;
+        },
+      },
+    });
+  };
+}
