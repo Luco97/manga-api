3510900d9b316a2a6fb3075b8ce3327c52298d10
Luco97 <renatoluco@gmail.com>
2022-01-20 22:47:41 -0300

    [ADD] se crea endpoint para obtener todos los mangas

--- src/manga/controller/manga.controller.ts
+++ src/manga/controller/manga.controller.ts
@@ -1,16 +1,27 @@
-import { Controller, UseGuards, Get, Res, HttpStatus } from '@nestjs/common';
+import { Manga, response } from '@interface/mangaResponses.interface';
+import { MangaService } from '@manga/services';
+import { Controller, UseGuards, Get, Res, HttpStatus, Post } from '@nestjs/common';
 import { Response } from 'express';
 import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('manga')
 @UseGuards(AuthGuard)
 export class MangaController {
-    constructor( ) {}
-    /* @Get('x')
-    getManga(@Res() res: Response) {
-        res.status(HttpStatus.OK)
-            .json({
-                message: 'Everithing works fine !!!'
-            })
-    } */
+  constructor(private _mangaService: MangaService) {}
+
+  @Get()
+  async allManga(@Res() res: Response<{response: response, data?: Manga[]}>) {
+    try {
+        const foo: {response: response, data: Manga[]} = await this._mangaService.getAll();
+        return res.status(foo.response.status).json(foo);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                  .json({
+                    response: {
+                      status: HttpStatus.INTERNAL_SERVER_ERROR,
+                      message: 'Error en el servidor'
+                    }
+                  });
+    }
+  }
 }
