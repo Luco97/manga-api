5c80c1a605f8e873214435a467b359d011f7afdc
Luco97 <renatoluco@gmail.com>
2022-01-20 22:47:17 -0300

    [ADD] se crea metodo para obtener todos los mangas

--- src/manga/services/manga.service.ts
+++ src/manga/services/manga.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+
+import { MangaEntity } from '@db/manga/entity';
+import { MangaEntityService } from '@db/manga/services';
+import { Manga, response } from '@interface/mangaResponses.interface';
+import { Injectable, HttpStatus } from '@nestjs/common';
 
 @Injectable()
-export class MangaService {}
+export class MangaService {
+    
+    constructor(
+        private _mangaService: MangaEntityService
+    ) {}
+
+    async getAll(): Promise<{response: response, data: Manga[]}> {
+        const data: MangaEntity[] = await this._mangaService.findAll([]);
+        if(data.length) {
+            return {
+                response: {
+                    status: HttpStatus.OK,
+                    message: 'getAll mangas'
+                },
+                data
+            }
+        }
+        return {
+            response: {
+                status: HttpStatus.OK,
+                message: 'Sin mangas'
+            },
+            data: []
+        }
+    }
+}
