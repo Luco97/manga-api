7e034d7332f308c9cc0a0b75b64c9df4b72708c9
Luco97 <renatoluco@gmail.com>
2022-01-22 18:10:17 -0300

    [ADD] se crea endpoint para crear manga

--- src/manga/controller/manga.controller.ts
+++ src/manga/controller/manga.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, UseGuards, Get, Res, HttpStatus, Post, Param, ParseIntPipe, Body } from '@nestjs/common';
 import { Response } from 'express';
 import { Manga, response } from '@interface/mangaResponses.interface';
-import { readMangaDto } from '@manga/dto';
+import { createMangaDto, readMangaDto } from '@manga/dto';
 import { MangaService } from '@manga/services';
 import { AuthGuard } from '../guards/auth.guard';
 
@@ -33,7 +33,26 @@ export class MangaController {
     @Res() res: Response<{response: response, data?: Manga}>
   ) {
     try {
-      const foo: {response: response, data?: Manga} = await this._mangaService.getOne(id, readManga);
+      const foo: {response: response, data?: Manga} = await this._mangaService.getOne(id, readManga.relations);
+      return res.status(foo.response.status).json(foo);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                  .json({
+                    response: {
+                      status: HttpStatus.INTERNAL_SERVER_ERROR,
+                      message: 'Error en el servidor'
+                    }
+                  });
+    }
+  }
+
+  @Post()
+  async createManga(
+    @Body() createManga: createMangaDto,
+    @Res() res: Response<{response: response, data?: Manga}>
+  ) {
+    try {
+      const foo: {response: response, data?: Manga} = await this._mangaService.create(createManga);
       return res.status(foo.response.status).json(foo);
     } catch (error) {
       return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
