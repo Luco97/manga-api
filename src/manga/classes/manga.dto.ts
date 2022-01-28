5f3fc30bb44a9e058ad891b9890c6a8de2d9fd81
Luco97 <renatoluco@gmail.com>
2022-01-28 18:30:11 -0300

    [ADD] se crea dto para actualizar, se mueve dto de lectura y se modifica mensaje en caso para canitdad de capitulos

--- src/manga/classes/manga.dto.ts
+++ src/manga/classes/manga.dto.ts
@@ -1,7 +1,19 @@
 import { mangaRelations } from '@db/manga/const';
 import { GenreEntity, ArtistEntity, LanguageEntity } from '@db/manga/entity';
+import { UserEntity } from '@userDB/entity';
 import { ArrayMinSize, ArrayNotEmpty, IsArray, IsNotEmpty, IsOptional, Length, Max, Min } from "class-validator";
 import { ArrayContainsSome } from './custom-validator/ArrayContainsSome.class-validator';
+
+export class readMangaDto {
+    @IsOptional()
+    @ArrayNotEmpty({
+        message: `Existe 'relations' pero se encuetra vacio`
+    })
+    @ArrayContainsSome( mangaRelations, {
+        message: 'Hay campos que no se encuentran definidos en la tabla consultada'
+    })
+    relations: string[];
+}
 
 export class createMangaDto {
     @IsNotEmpty({
@@ -15,11 +27,11 @@ export class createMangaDto {
     @IsNotEmpty({
         message: `Falta definir la propiedad 'chapters'`
     })
-    @Min( 2, {
-        message: 'Numero de paginas bajo el minimo'
+    @Min( 1, {
+        message: 'Numero de capitulos bajo el minimo'
     })
     @Max( 1000, {
-        message: 'Numero de paginas sobre el maximo'
+        message: 'Numero de capitulos sobre el maximo'
     })
     chapters: number;
 
@@ -58,13 +70,70 @@ export class createMangaDto {
 
 }
 
-export class readMangaDto {
+export class updateMangaDto {
     @IsOptional()
-    @ArrayNotEmpty({
-        message: `Existe 'relations' pero se encuetra vacio`
+    @IsNotEmpty({
+        message: `Falta definir la propiedad 'title'`
     })
-    @ArrayContainsSome( mangaRelations, {
-        message: 'Hay campos que no se encuentran definidos en la tabla consultada'
+    @Length( 5, 150, {
+        message: 'Esta bajo el minimo/Supera el maximo de caracteres'
     })
-    relations: string[];
+    title: string;
+
+    @IsOptional()
+    @Min( 1, {
+        message: 'Numero de capitulos bajo el minimo'
+    })
+    @Max( 1000, {
+        message: 'Numero de capitulos sobre el maximo'
+    })
+    chapters: number;
+
+    @IsOptional()
+    @IsArray({
+        message: `Propiedad 'genres' no es de tipo array`
+    })
+    @IsNotEmpty({
+        message: `Falta definir la propiedad 'genres'`
+    })
+    @ArrayMinSize( 1, {
+        message: `array 'genres' debe contener como minimo un elemento`
+    })
+    genres: GenreEntity[];
+
+    @IsOptional()
+    @IsArray({
+        message: `Propiedad 'artists' no es de tipo array`
+    })
+    @IsNotEmpty({
+        message: `Falta definir la propiedad 'artists'`
+    })
+    @ArrayMinSize( 1, {
+        message: `array 'artists' debe contener como minimo un elemento`
+    })
+    artists: ArtistEntity[];
+
+    @IsOptional()
+    @IsArray({
+        message: `Propiedad 'languages' no es de tipo array`
+    })
+    @IsNotEmpty({
+        message: `Falta definir la propiedad 'languages'`
+    })
+    @ArrayMinSize( 1, {
+        message: `array 'languages' debe contener como minimo un elemento`
+    })
+    languages: LanguageEntity[];
+
+    @IsOptional()
+    @IsArray({
+        message: `Propiedad 'languages' no es de tipo array`
+    })
+    @IsNotEmpty({
+        message: `Falta definir la propiedad 'languages'`
+    })
+    @ArrayMinSize( 1, {
+        message: `array 'languages' debe contener como minimo un elemento`
+    })
+    users: UserEntity[];
 }
