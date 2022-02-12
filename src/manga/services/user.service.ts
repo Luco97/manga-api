2eefc7eafa3b7ac0868b9c1762282e49e1af3303
Luco97 <renatoluco@gmail.com>
2022-02-12 20:50:48 -0300

    [ADD] se crea logica para seteo de mangas favoritos para usuarios

--- src/manga/services/user.service.ts
+++ src/manga/services/user.service.ts
@@ -1,4 +1,6 @@
+import { MangaEntity } from '@db/manga/entity';
 import { response, User } from '@interface/mangaResponses.interface';
+import { setFavorite } from '@manga/dto';
 import { Injectable, HttpStatus } from '@nestjs/common';
 import { UserEntity } from '@userDB/entity';
 import { UserEntityService } from '@userDB/service';
@@ -35,10 +37,43 @@ export class UserService {
         }
     }
 
-    async setFavorite(id: number): Promise<{response: response, data?: User}> {
+    async setFavorite(id: number, body: setFavorite): Promise<{response: response, data?: User}> {
         const data: {response: response, data?: User} = await this.getOne(id);
         if(data?.data) {
-            //Seteando favorito, pensando logica
+            const user: UserEntity = data.data as UserEntity;
+            const manga = data.data.mangas.find( manga => manga.id == body.manga.id);
+            //Manga ya existe como favorito
+            if(manga) {
+                user.mangas = data.data.mangas.splice(1, 0, manga);
+                await this._userService.create(user);
+                return {
+                    response: {
+                        status: HttpStatus.OK,
+                        message: `Manga con id = '${body.manga.id}' y titulo = '${body.manga.title}' ya existe, Manga eliminado`
+                    },
+                    data: user
+                };
+            }
+            user.mangas.push(body.manga as MangaEntity);
+            
+            try {
+                await this._userService.create(user);
+            } catch (error) {
+                //Manga incorrecto/no existe
+                return {
+                    response: {
+                        status: HttpStatus.BAD_REQUEST,
+                        message: `Manga con id = '${body.manga.id}' no existe`
+                    }
+                };
+            }
+            return {
+                response: {
+                    status: HttpStatus.OK,
+                    message: 'setFavorite user'
+                },
+                data: user
+            };
         }
         return {
             response: {
