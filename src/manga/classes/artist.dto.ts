16e4da13e848342e99efa5356d00672085d761bc
Luco97 <renatoluco@gmail.com>
2022-02-14 19:19:58 -0300

    [CHG] se usa extend para agregar elementos de paginacion

--- src/manga/classes/artist.dto.ts
+++ src/manga/classes/artist.dto.ts
@@ -1,9 +1,10 @@
 import { artistRelations } from '@db/manga/const';
 import { ArrayContainsSome } from './custom-validator/ArrayContainsSome.class-validator';
 import { ArrayNotEmpty, IsNotEmpty, IsOptional, Length, Max, Min } from 'class-validator';
+import { Pagination } from './utils.dto';
 
 
-export class readArtistDto {
+export class readArtistDto extends Pagination{
     @IsOptional()
     @ArrayNotEmpty({
         message: `No se encuentran definido 'relations', si no quiere relaciones ingresar 'relations: []'`
