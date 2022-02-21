e7857600fc0560b9279067d4813a59e7e309002d
Luco97 <renatoluco@gmail.com>
2022-02-21 19:23:28 -0300

    [CHG] se formatea archivo

--- src/db/manga-entities/const/relations.ts
+++ src/db/manga-entities/const/relations.ts
@@ -4,4 +4,9 @@ export const genreRelations: string[] = ['mangas'];
 
 export const languageRelations: string[] = ['mangas'];
 
-export const mangaRelations: string[] = ['genres', 'languages', 'artists', 'users'];
+export const mangaRelations: string[] = [
+  'genres',
+  'languages',
+  'artists',
+  'users',
+];
