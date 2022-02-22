0c3d03bd494ba30237b71156ebcdbc97e8de34ae
Luco97 <renatoluco@gmail.com>
2022-02-22 20:55:12 -0300

    [ADD] se quita middleware de login y se cambia por payload para obtener objeto de usuario en base al token

--- src/manga/middleware/login.middleware.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response } from 'express';
-
-@Injectable()
-export class LoginMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: () => void) {
-    next();
-  }
-}
---
+++ src/manga/middleware/payload.middleware.ts
@@ -0,0 +1,19 @@
+import { Injectable, NestMiddleware, Body } from '@nestjs/common';
+import { JwtService } from '@shared/services';
+import { Request, Response } from 'express';
+
+@Injectable()
+export class PayloadMiddleware implements NestMiddleware {
+
+  constructor(
+    private jwtService: JwtService
+  ) {}
+
+  use(req: Request, res: Response, next: () => void) {
+    req.body = {
+      user: this.jwtService.getObjectJWT(req.headers.authorization?.replace('Bearer ', '')),
+      ...req.body
+    } 
+    next();
+  }
+}
