0ecac6c736f8eb00e6a25e7872520b28ed6aca59
Luco97 <renatoluco@gmail.com>
2022-03-01 22:30:40 -0300

    [ADD] se agrega implementacion cuando se inicia una conexion con el servidor, se emite un manga (con nombre de evento dinamico, es decir, depende de la id del manga) cuando fue agregado a favoritos de algun usario

--- src/manga/events/events.gateway.ts
+++ src/manga/events/events.gateway.ts
@@ -1,29 +1,32 @@
+import { Manga } from '@interface/mangaResponses.interface';
 import { MangaService } from '@manga/services';
-import { ConnectedSocket, MessageBody, OnGatewayConnection, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayInit, WebSocketGateway } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
 
 @WebSocketGateway()
-export class EventsGateway implements OnGatewayConnection {
+export class EventsGateway implements OnGatewayConnection, OnGatewayInit {
 
   constructor(
     private _mangaService: MangaService
   ) {}
+
+  afterInit(server: Server) {
+    this._mangaService.mangaFavoriteSubject.subscribe( ({response, data}) => {
+      server.emit(`favorite:manga:${data.id}`, data);
+    })
+  }
   
   handleConnection(client: Socket, ...args: any[]) {
 
-    this._mangaService.mangaSubject.subscribe( ({ response, data }) => {
-      client.emit('ost', response);
+    //listener que emite los mangas que fueron creados 
+    //desde que alguien a iniciado una conexion con el servidor
+    this._mangaService.mangaCreateSubject.subscribe( ({ response, data }) => {
+      client.emit('manga-create-controller', {
+        response,
+        data
+      });
     });
 
   }
 
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): { numero: number; palabra: string} {
-    console.log('--> client!!' , client);
-    console.log('--> payload!!' , payload);
-    return {
-      numero: 10,
-      palabra: 'un string'
-    };
-  }
 }
