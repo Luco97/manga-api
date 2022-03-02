706df1667f3ac4397a1956596a0e4da0d5886f05
Luco97 <renatoluco@gmail.com>
2022-03-02 19:58:24 -0300

    [ADD] se crea servicio para mantener los Subject

---
+++ src/manga/services/utils.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { Subject } from 'rxjs';
+import { Manga, response } from '@interface/mangaResponses.interface';
+
+@Injectable()
+export class UtilsService {
+  mangaCreateSubject: Subject<{ response: response; data?: Manga }> =
+    new Subject();
+  mangaFavoriteSubject: Subject<{ response: response; data?: Manga }> =
+    new Subject();
+}
