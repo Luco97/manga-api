0439483802e87caadd9ef37074eca256be13f9ae
Luco97 <renatoluco@gmail.com>
2022-03-04 23:55:40 -0300

    [CHG] se emiten mas daatos por el socket (manga y respuesta)

--- src/manga/events/events.gateway.ts
+++ src/manga/events/events.gateway.ts
@@ -1,18 +1,20 @@
-import { Manga } from '@interface/mangaResponses.interface';
-import { MangaService } from '@manga/services';
 import { OnGatewayConnection, OnGatewayInit, WebSocketGateway } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
+import { UtilsService } from '@manga/services';
 
 @WebSocketGateway()
 export class EventsGateway implements OnGatewayConnection, OnGatewayInit {
 
   constructor(
-    private _mangaService: MangaService
+    private _utilsService: UtilsService
   ) {}
 
   afterInit(server: Server) {
-    this._mangaService.mangaFavoriteSubject.subscribe( ({response, data}) => {
-      server.emit(`favorite:manga:${data.id}`, data);
+    this._utilsService.mangaFavoriteSubject.subscribe( ({response, data}) => {
+      server.emit(`favorite:manga:${data.id}`, {
+        response,
+        data
+      });
     })
   }
   
@@ -20,7 +22,7 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayInit {
 
     //listener que emite los mangas que fueron creados 
     //desde que alguien a iniciado una conexion con el servidor
-    this._mangaService.mangaCreateSubject.subscribe( ({ response, data }) => {
+    this._utilsService.mangaCreateSubject.subscribe( ({ response, data }) => {
       client.emit('manga-create-controller', {
         response,
         data
