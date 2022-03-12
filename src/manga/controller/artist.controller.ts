51be5ce5692d49a2235ebcf9cea2d295cd245cc1
Luco97 <renatoluco@gmail.com>
2022-03-12 17:06:21 -0300

    [ADD] se formatea codigo y se crea endpoint para obtener mangas en base a un id de artista para obtener sus mangas asociados (con paginacion)

--- src/manga/controller/artist.controller.ts
+++ src/manga/controller/artist.controller.ts
@@ -1,113 +1,144 @@
-import { Controller, Res, HttpStatus, UseGuards, Param, ParseIntPipe, Post, Body, Put, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Res,
+  HttpStatus,
+  UseGuards,
+  Param,
+  ParseIntPipe,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { Artist, response } from '@interface/mangaResponses.interface';
 import { ArtistService } from '@manga/services';
-import { createArtistDto, readArtistDto, updateArtistsDto } from '@manga/dto';
+import {
+  createArtistDto,
+  readArtistDto,
+  readArtist_GetMangaDto,
+  updateArtistsDto,
+} from '@manga/dto';
 import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('artist')
 @UseGuards(AuthGuard)
 export class ArtistController {
+  constructor(private _artistService: ArtistService) {}
 
-    constructor(
-        private _artistService: ArtistService
-    ) {}
+  @Post()
+  async getAll(
+    @Body() bodyArtists: readArtistDto,
+    @Res() res: Response<{ response: response; data?: Artist[] }>,
+  ) {
+    try {
+      const foo: { response: response; data?: Artist[] } =
+        await this._artistService.getAll(bodyArtists);
+      return res.status(foo.response.status).json(foo);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        response: {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Error en el servidor',
+        },
+      });
+    }
+  }
 
-    @Post()
-    async getAll(
-        @Body() bodyArtists: readArtistDto,
-        @Res() res: Response<{response: response, data?: Artist[]}>
-    ) {
-        try {
-            const foo: {response: response, data?: Artist[]} = await this._artistService.getAll(bodyArtists);
-            return res.status(foo.response.status)
-                        .json(foo);
-        } catch (error) {
-            return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .json({
-                            response:{
-                                status: HttpStatus.INTERNAL_SERVER_ERROR,
-                                message: 'Error en el servidor'
-                            }
-                        })
-        }
+  @Post('create')
+  async create(
+    @Body() createArtist: createArtistDto,
+    @Res() res: Response<{ response: response; data?: Artist }>,
+  ) {
+    try {
+      const foo: { response: response; data?: Artist } =
+        await this._artistService.create(createArtist);
+      return res.status(foo.response.status).json(foo);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        response: {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Error en el servidor',
+        },
+      });
     }
-    
-    @Post('create')
-    async create(
-        @Body() createArtist: createArtistDto,
-        @Res() res: Response<{response: response, data?: Artist}>
-    ) {
-        try {
-            const foo: {response: response, data?: Artist} = await this._artistService.create(createArtist);
-            return res.status(foo.response.status)
-                        .json(foo);
-                    } catch (error) {
-                        return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .json({
-                            response: {
-                                status: HttpStatus.INTERNAL_SERVER_ERROR,
-                                message: 'Error en el servidor'
-                            }
-                        })
-        }
-    }
-    
-    @Put('update/:id')
-    async update(
-        @Param('id', ParseIntPipe) id: number,
-        @Body() updateArtist: updateArtistsDto,
-        @Res() res: Response<{ response: response, data?: Artist }>
-        ) {
-            try {
-            const foo: {response: response, data?: Artist} = await this._artistService.update(id, updateArtist);
-            return res.status(foo.response.status).json(foo);
-        } catch (error) {
-            return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .json({
-                response: {
-                    status: HttpStatus.INTERNAL_SERVER_ERROR,
-                    message: 'Error en el servidor'
-                }
-            })
-        }
-    }
-    
-    @Delete('delete/:id')
-    async delete(
-        @Param('id', ParseIntPipe) id: number,
-        @Res() res: Response<{response: response, data?: Artist}>
-        ){
-            try {
-                const foo: {response: response, data?: Artist} = await this._artistService.delete(id);
-            } catch (error) {
-                return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .json({
-                response: {
-                    status: HttpStatus.INTERNAL_SERVER_ERROR,
-                    message: 'Error en el servidor'
-                }
-            })   
-        }
-    }
+  }
 
-    @Put(':id')
-    async findOne(
-        @Param('id', ParseIntPipe) id: number, 
-        @Body() readArtist: readArtistDto,
-        @Res() res: Response<{response: response, data?: Artist}>
-    ) {
-        try {
-            const foo: {response: response, data?: Artist} = await this._artistService.getOne(id, readArtist.relations);
-            return res.status(foo.response.status).json(foo);
-        } catch (error) {
-            return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .json({
-                            response:{
-                                status: HttpStatus.INTERNAL_SERVER_ERROR,
-                                message: 'Error en el servidor'
-                            }
-                        })
-        }
+  @Put('update/:id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateArtist: updateArtistsDto,
+    @Res() res: Response<{ response: response; data?: Artist }>,
+  ) {
+    try {
+      const foo: { response: response; data?: Artist } =
+        await this._artistService.update(id, updateArtist);
+      return res.status(foo.response.status).json(foo);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        response: {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Error en el servidor',
+        },
+      });
     }
+  }
+
+  @Delete('delete/:id')
+  async delete(
+    @Param('id', ParseIntPipe) id: number,
+    @Res() res: Response<{ response: response; data?: Artist }>,
+  ) {
+    try {
+      const foo: { response: response; data?: Artist } =
+        await this._artistService.delete(id);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        response: {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Error en el servidor',
+        },
+      });
+    }
+  }
+
+  @Post(':id/manga')
+  async getMangasByArtist(
+    @Body() bodyArtists: readArtist_GetMangaDto,
+    @Param('id', ParseIntPipe) id: number,
+    @Res() res: Response<{ response: response; data?: Artist }>,
+  ) {
+    try {
+      const foo: { response: response; data?: Artist } =
+        await this._artistService.getArtistMangas(id, bodyArtists);
+      return res.status(foo.response.status).json(foo);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        response: {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Error en el servidor',
+        },
+      });
+    }
+  }
+
+  @Put(':id')
+  async findOne(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() readArtist: readArtistDto,
+    @Res() res: Response<{ response: response; data?: Artist }>,
+  ) {
+    try {
+      const foo: { response: response; data?: Artist } =
+        await this._artistService.getOne(id, readArtist.relations);
+      return res.status(foo.response.status).json(foo);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        response: {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Error en el servidor',
+        },
+      });
+    }
+  }
 }
