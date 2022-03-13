8386569f39a82303083d029d75e552affe658a55
Luco97 <renatoluco@gmail.com>
2022-03-13 01:32:09 -0300

    [ADD] se agrega dto para obtener mangas de un genero y se agrega una paginacion

--- src/manga/classes/genre.dto.ts
+++ src/manga/classes/genre.dto.ts
@@ -1,21 +1,28 @@
 import { IsNotEmpty, Length } from 'class-validator';
-import { genreRelations } from "@db/manga/const";
-import { ArrayContainsSome } from "./custom-validator/ArrayContainsSome.class-validator";
+import { genreRelations, mangaRelations } from '@db/manga/const';
+import { ArrayContainsSome } from './custom-validator/ArrayContainsSome.class-validator';
 import { Pagination } from './utils.dto';
 
 export class createGenreDto {
-    @IsNotEmpty({
-        message: `Falta la propiedad 'tag'`
-    })
-    @Length( 1, 30, {
-        message: 'Esta bajo el minimo/Supera el maximo de caracteres'
-    })
-    tag: string;
+  @IsNotEmpty({
+    message: `Falta la propiedad 'tag'`,
+  })
+  @Length(1, 30, {
+    message: 'Esta bajo el minimo/Supera el maximo de caracteres',
+  })
+  tag: string;
 }
 
-export class readGenreDto extends Pagination{
-    @ArrayContainsSome(genreRelations, {
-        message: 'Hay campos que no se encuentran definidos en la tabla consultada'
-    })
-    relations: string[];
+export class readGenreDto extends Pagination {
+  @ArrayContainsSome(genreRelations, {
+    message: 'Hay campos que no se encuentran definidos en la tabla consultada',
+  })
+  relations: string[];
+}
+
+export class readGenre_getMangasDto extends Pagination {
+  @ArrayContainsSome(mangaRelations, {
+    message: 'Hay campos que no se encuentran definidos en la tabla consultada',
+  })
+  relations: string[];
 }
