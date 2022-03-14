f9a605871938cd03871b28ccf7dd9e36b0c8958e
Luco97 <renatoluco@gmail.com>
2022-03-14 20:03:13 -0300

    [CHG] se formatea codigo y se quita endpoint comentado

--- src/manga/controller/user.controller.ts
+++ src/manga/controller/user.controller.ts
@@ -1,8 +1,15 @@
-import { Controller, HttpStatus, Res, UseGuards, Post, Param, ParseIntPipe, Body, Put } from '@nestjs/common';
+import {
+  Controller,
+  HttpStatus,
+  Res,
+  UseGuards,
+  Post,
+  Body,
+  Put,
+} from '@nestjs/common';
 import { Response } from 'express';
 
 import { response, User } from '@interface/mangaResponses.interface';
-import { UserEntityService } from '@db/user/service';
 import { getFavorite, setFavorite } from '@manga/dto';
 import { UserService } from '@manga/services';
 import { AuthGuard } from '../guards/auth.guard';
@@ -10,82 +17,52 @@ import { AuthGuard } from '../guards/auth.guard';
 @Controller('user')
 @UseGuards(AuthGuard)
 export class UserController {
+  constructor(
+    private _userService: UserService /* UserEntityService */,
+  ) {}
 
-    constructor(
-        private _userService: UserService /* UserEntityService */,
-        private _userEntity: UserEntityService
-    ){}
-
-    /* @Get()
-    async getAll(
-        @Res() res: Response<{response: response, users?: User[]}>
-    ) {
-        try {
-            const data: UserEntity[] = await this._userEntity.findBy({
-                select: ['id', 'username'],
-                relations: ['mangas']
-            });
-            return res.status(200).json({
-                response: {
-                    status: 200,
-                    message: 'Probando get all de usuarios, esperando no ver password'
-                },
-                users: data
-            })
-        } catch (error) {
-            return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .json({
-                            response: {
-                                status: HttpStatus.INTERNAL_SERVER_ERROR,
-                                message: 'Error en el servidor'
-                            }
-                        })
-        }
-    } */
-
-    @Post('favorites')
-    async getFavorites(
-        @Body() getBody: getFavorite,
-        @Res() res: Response<{
-            response: response;
-            data?: User;
-        }>
-    ) {
-        try {
-            const foo: { response: response, data?: User; } = await this._userService.getFavorites(getBody);
-            return res.status(foo.response.status).json(foo);   
-        } catch (error) {
-            return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .json({
-                            response: {
-                                status: HttpStatus.INTERNAL_SERVER_ERROR,
-                                message: 'Error en el servidor'
-                            }
-                        })
-        }
+  @Post('favorites')
+  async getFavorites(
+    @Body() getBody: getFavorite,
+    @Res()
+    res: Response<{
+      response: response;
+      data?: User;
+    }>,
+  ) {
+    try {
+      const foo: { response: response; data?: User } =
+        await this._userService.getFavorites(getBody);
+      return res.status(foo.response.status).json(foo);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        response: {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Error en el servidor',
+        },
+      });
     }
+  }
 
-    @Put('favorites')
-    async favoriteManga(
-        //@Param('id', ParseIntPipe) id: number,
-        @Body() newFavorite: setFavorite,
-        @Res() res: Response<{
-            response: response;
-            data?: User;
-        }>
-    ) {
-        try {
-            const { id } = newFavorite.user
-            const foo: {response: response, data?: User} = await this._userService.setFavorite(id, newFavorite);
-            return res.status(foo.response.status).json(foo);
-        } catch (error) {
-            return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .json({
-                            response: {
-                                status: HttpStatus.INTERNAL_SERVER_ERROR,
-                                message: 'Error en el servidor'
-                            }
-                        })
-        }
+  @Put('favorites')
+  async favoriteManga(
+    //@Param('id', ParseIntPipe) id: number,
+    @Body() newFavorite: setFavorite,
+    @Res()
+    res: Response<{ response: response; data?: User }>,
+  ) {
+    try {
+      const { id } = newFavorite.user;
+      const foo: { response: response; data?: User } =
+        await this._userService.setFavorite(id, newFavorite);
+      return res.status(foo.response.status).json(foo);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        response: {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Error en el servidor',
+        },
+      });
     }
+  }
 }
