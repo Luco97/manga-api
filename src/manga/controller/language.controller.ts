724d81e6d5a9409dd6b8ba737f840ba1e99a2f5a
Luco97 <renatoluco@gmail.com>
2022-03-14 20:04:01 -0300

    [CHG] se formatea controlador de idioma

--- src/manga/controller/language.controller.ts
+++ src/manga/controller/language.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, HttpStatus, Param, Post, Res, UseGuards, ParseIntPipe, Body, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+  UseGuards,
+  ParseIntPipe,
+  Body,
+  Delete,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { Language, response } from '@interface/mangaResponses.interface';
 import { createLanguageDto, readLanguageDto } from '@manga/dto';
@@ -8,85 +19,81 @@ import { AuthGuard } from '../guards/auth.guard';
 @UseGuards(AuthGuard)
 @Controller('language')
 export class LanguageController {
+  constructor(private _languageService: LanguageService) {}
 
-    constructor(
-        private _languageService: LanguageService
-    ) {}
+  @Get()
+  async getAll(
+    @Res() res: Response<{ response: response; data?: Language[] }>,
+  ) {
+    try {
+      const foo: { response: response; data?: Language[] } =
+        await this._languageService.getAll();
+      return res.status(foo.response.status).json(foo);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        response: {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Error en el servidor',
+        },
+      });
+    }
+  }
 
-    @Get()
-    async getAll(
-        @Res() res: Response<{response: response, data?: Language[]}>
-    ) {
-        try {
-            const foo: {response: response, data?: Language[]} = await this._languageService.getAll();
-            return res.status(foo.response.status).json(foo);
-        } catch (error) {
-            return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .json({
-                            response: {
-                                status: HttpStatus.INTERNAL_SERVER_ERROR,
-                                message: 'Error en el servidor'
-                            }
-                        })
-        }
+  @Post('create')
+  async create(
+    @Body() createLanguage: createLanguageDto,
+    @Res() res: Response<{ response: response; data?: Language }>,
+  ) {
+    try {
+      const foo: { response: response; data?: Language } =
+        await this._languageService.create(createLanguage);
+      return res.status(foo.response.status).json(foo);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        response: {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Error en el servidor',
+        },
+      });
     }
+  }
 
-    @Post('create')
-    async create(
-        @Body() createLanguage: createLanguageDto,
-        @Res() res: Response<{response: response, data?: Language}>
-    ) {
-        try {
-            const foo: {response: response, data?: Language} = await this._languageService.create(createLanguage);
-            return res.status(foo.response.status).json(foo);
-        } catch (error) {
-            return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .json({
-                            response: {
-                                status: HttpStatus.INTERNAL_SERVER_ERROR,
-                                message: 'Error en el servidor'
-                            }
-                        })
-        }
+  @Delete('delete/:id')
+  async delete(
+    @Param('id', ParseIntPipe) id: number,
+    @Res() res: Response<{ response: response; data?: Language }>,
+  ) {
+    try {
+      const foo: { response: response; data?: Language } =
+        await this._languageService.delete(id);
+      return res.status(foo.response.status).json(foo);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        response: {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Error en el servidor',
+        },
+      });
     }
+  }
 
-    @Delete('delete/:id')
-    async delete(
-        @Param('id', ParseIntPipe) id: number,
-        @Res() res: Response<{response: response, data?: Language}>
-    ) {
-        try {
-            const foo: {response: response, data?: Language} = await this._languageService.delete(id);
-            return res.status(foo.response.status).json(foo);
-        } catch (error) {
-            return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .json({
-                            response: {
-                                status: HttpStatus.INTERNAL_SERVER_ERROR,
-                                message: 'Error en el servidor'
-                            }
-                        })
-        }
+  @Post(':id')
+  async getOne(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() readLanguage: readLanguageDto,
+    @Res() res: Response<{ response: response; data?: Language }>,
+  ) {
+    try {
+      const foo: { response: response; data?: Language } =
+        await this._languageService.getOne(id, readLanguage.relations);
+      return res.status(foo.response.status).json(foo);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        response: {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Error en el servidor',
+        },
+      });
     }
-    
-    @Post(':id')
-    async getOne(
-        @Param('id',ParseIntPipe) id: number,
-        @Body() readLanguage: readLanguageDto,
-        @Res() res: Response<{response: response, data?: Language}>
-    ) {
-        try {
-            const foo: {response: response, data?: Language} = await this._languageService.getOne(id, readLanguage.relations);
-            return res.status(foo.response.status).json(foo);
-        } catch (error) {
-            return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .json({
-                            response: {
-                                status: HttpStatus.INTERNAL_SERVER_ERROR,
-                                message: 'Error en el servidor'
-                            }
-                        })
-        }
-    }
+  }
 }
-
