e7638af52f2bb7b743f0761976b57c7ed32b70bf
Luco97 <renatoluco@gmail.com>
2022-03-15 00:16:47 -0300

    [CHG] se formatea codigo de sockets

--- src/manga/events/events.gateway.ts
+++ src/manga/events/events.gateway.ts
@@ -1,40 +1,38 @@
-import { OnGatewayConnection, OnGatewayInit, WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayInit,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
 import { UtilsService } from '@manga/services';
 
 @WebSocketGateway()
 export class EventsGateway implements OnGatewayConnection, OnGatewayInit {
-
-  constructor(
-    private _utilsService: UtilsService
-  ) {}
+  constructor(private _utilsService: UtilsService) {}
 
   afterInit(server: Server) {
-    this._utilsService.mangaFavoriteSubject.subscribe( ({response, data}) => {
+    this._utilsService.mangaFavoriteSubject.subscribe(({ response, data }) => {
       server.emit(`favorite:manga:${data.id}`, {
         response,
-        data
-      });
-    })
-    this._utilsService.mangaDropSubject.subscribe( ({response, data}) => {
-      server.emit(`favorite:drop:manga:${data.id}`, {
-        response,
-        data
-      })
-    })
-  }
-  
-  handleConnection(client: Socket, ...args: any[]) {
-
-    //listener que emite los mangas que fueron creados 
-    //desde que alguien a iniciado una conexion con el servidor
-    this._utilsService.mangaCreateSubject.subscribe( ({ response, data }) => {
-      client.emit('manga:create:controller', {
-        response,
-        data
+        data,
       });
     });
-
+    this._utilsService.mangaDropSubject.subscribe(({ response, data }) => {
+      server.emit(`favorite:drop:manga:${data.id}`, {
+        response,
+        data,
+      });
+    });
   }
 
+  handleConnection(client: Socket, ...args: any[]) {
+    //listener que emite los mangas que fueron creados
+    //desde que alguien a iniciado una conexion con el servidor
+    this._utilsService.mangaCreateSubject.subscribe(({ response, data }) => {
+      client.emit('manga:create:controller', {
+        response,
+        data,
+      });
+    });
+  }
 }
