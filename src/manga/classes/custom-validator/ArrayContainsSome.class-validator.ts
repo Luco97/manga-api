ed74a241386167753312746bedf05382216b88bb
Luco97 <renatoluco@gmail.com>
2022-03-15 00:16:17 -0300

    [CHG] se formatea codigo para validar si existe dentro del conjunto

--- src/manga/classes/custom-validator/ArrayContainsSome.class-validator.ts
+++ src/manga/classes/custom-validator/ArrayContainsSome.class-validator.ts
@@ -1,6 +1,13 @@
-import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator';
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidationArguments,
+} from 'class-validator';
 
-export function ArrayContainsSome(property: string[], validationOptions?: ValidationOptions) {
+export function ArrayContainsSome(
+  property: string[],
+  validationOptions?: ValidationOptions,
+) {
   return function (object: Object, propertyName: string) {
     registerDecorator({
       name: 'ArrayContainsSome',
@@ -9,12 +16,11 @@ export function ArrayContainsSome(property: string[], validationOptions?: Valida
       options: validationOptions,
       validator: {
         validate(value: string[], args: ValidationArguments) {
-            for (let index = 0; index < value?.length; index++) {
-                const element = value[index];
-                if(!property.includes(element)) 
-                    return false;
-            }
-            return true;
+          for (let index = 0; index < value?.length; index++) {
+            const element = value[index];
+            if (!property.includes(element)) return false;
+          }
+          return true;
         },
       },
     });
