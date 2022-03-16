b82fd5f26383cbcf811e53be3a2c7d710a5d4e66
Luco97 <renatoluco@gmail.com>
2022-03-16 18:47:51 -0300

    [CHG] se formatea dto's de usuario

--- src/manga/classes/user.dto.ts
+++ src/manga/classes/user.dto.ts
@@ -1,30 +1,29 @@
 import { mangaRelations } from '@db/manga/const';
 import { Manga } from '@interface/mangaResponses.interface';
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty } from 'class-validator';
 import { ArrayContainsSome } from './custom-validator/ArrayContainsSome.class-validator';
 import { Pagination } from './utils.dto';
 
 export class setFavorite {
-    
-    user: {
-        id: number;
-        username: string;
-    }
+  user: {
+    id: number;
+    username: string;
+  };
 
-    @IsNotEmpty({
-        message: `Falta definir la propiedad 'manga'`
-    })
-    manga: Manga;
+  @IsNotEmpty({
+    message: `Falta definir la propiedad 'manga'`,
+  })
+  manga: Manga;
 }
 
 export class getFavorite extends Pagination {
-    user: {
-        id: number;
-        username: string;
-    }
+  user: {
+    id: number;
+    username: string;
+  };
 
-    @ArrayContainsSome( mangaRelations, {
-        message: 'Hay campos que no se encuentran definidos en la tabla consultada'
-    })
-    relations: string[];
+  @ArrayContainsSome(mangaRelations, {
+    message: 'Hay campos que no se encuentran definidos en la tabla consultada',
+  })
+  relations: string[];
 }
