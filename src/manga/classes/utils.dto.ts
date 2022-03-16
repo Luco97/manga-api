062ad5a3eb0ead31279fa4e8645bbcc2346508e9
Luco97 <renatoluco@gmail.com>
2022-03-16 18:47:25 -0300

    [CHG] se formatea dto de paginacion

--- src/manga/classes/utils.dto.ts
+++ src/manga/classes/utils.dto.ts
@@ -1,15 +1,15 @@
-import { IsOptional, Min } from "class-validator";
+import { IsOptional, Min } from 'class-validator';
 
 export class Pagination {
-    @IsOptional()
-    @Min(1, {
-        message: 'Valor no puede ser menor que 1'
-    })
-    take: number;
-    
-    @IsOptional()
-    @Min(0, {
-        message: 'Valor no puede ser menor a 0'
-    })
-    skip: number;
+  @IsOptional()
+  @Min(1, {
+    message: 'Valor no puede ser menor que 1',
+  })
+  take: number;
+
+  @IsOptional()
+  @Min(0, {
+    message: 'Valor no puede ser menor a 0',
+  })
+  skip: number;
 }
