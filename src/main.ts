40aede1498ba496c201a9350b297a6540d9d2a22
Luco97 <renatoluco@gmail.com>
2022-03-16 22:37:11 -0300

    [CHG] se definen variables para aplicacion en heroku

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 8080;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(process.env.PORT);
+  await app.listen(port);
 }
 bootstrap();
