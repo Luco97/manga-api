45a39721ecc3a5409562d6055a36dad1de188f9d
Luco97 <renatoluco@gmail.com>
2022-03-18 18:41:20 -0300

    [CHG] se configura para deploy con heroku

--- src/shared/services/db-config.service.ts
+++ src/shared/services/db-config.service.ts
@@ -5,18 +5,19 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 @Injectable()
 export class DbConfigService {
   getTypeORMconfig(entity: string = '/../**/*.entity.ts'): TypeOrmModuleOptions {
-    console.log(__dirname)
     return {
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
+       type: 'postgres',
+      
+      // host: process.env.POSTGRES_HOST,
+      // port: parseInt(process.env.POSTGRES_PORT),
+      // username: process.env.POSTGRES_USER,
+      // password: process.env.POSTGRES_PASSWORD,
+      // database: process.env.POSTGRES_DATABASE, 
+      url: process.env.DATABASE_URL,
       entities: [
         __dirname + entity
       ],
-      synchronize: true,
+      //synchronize: true,
       autoLoadEntities: true,
       //logging: ['query'],
       ssl: true,
