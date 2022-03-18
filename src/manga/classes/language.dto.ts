f2495ab603f50b671103ea2de0e2da7c5007a1f6
Luco97 <renatoluco@gmail.com>
2022-03-18 18:04:04 -0300

    [CHG] se formatea dto's para idiomas

--- src/manga/classes/language.dto.ts
+++ src/manga/classes/language.dto.ts
@@ -1,31 +1,36 @@
-import { languageRelations } from "@db/manga/const";
-import { ArrayContainsSome } from "./custom-validator/ArrayContainsSome.class-validator";
-import { ArrayContains, ArrayNotEmpty, IsNotEmpty, IsOptional, Length } from "class-validator";
+import { languageRelations } from '@db/manga/const';
+import { ArrayContainsSome } from './custom-validator/ArrayContainsSome.class-validator';
+import {
+  ArrayNotEmpty,
+  IsNotEmpty,
+  IsOptional,
+  Length,
+} from 'class-validator';
 
 export class createLanguageDto {
-    @IsNotEmpty({
-        message: `Falta definir la propiedad 'language'`
-    })
-    @Length( 1, 30, {
-        message: 'Esta bajo el minimo/Supera el maximo de caracteres'
-    })
-    language: string;
-    @IsNotEmpty({
-        message: `Falta definir la propiedad 'country'`
-    })
-    @Length( 1, 30, {
-        message: 'Esta bajo el minimo/Supera el maximo de caracteres'
-    })
-    country: string;
+  @IsNotEmpty({
+    message: `Falta definir la propiedad 'language'`,
+  })
+  @Length(1, 30, {
+    message: 'Esta bajo el minimo/Supera el maximo de caracteres',
+  })
+  language: string;
+  @IsNotEmpty({
+    message: `Falta definir la propiedad 'country'`,
+  })
+  @Length(1, 30, {
+    message: 'Esta bajo el minimo/Supera el maximo de caracteres',
+  })
+  country: string;
 }
 
 export class readLanguageDto {
-    @IsOptional()
-    @ArrayNotEmpty({
-        message: `Existe 'relations' pero se encuetra vacio`
-    })
-    @ArrayContainsSome(languageRelations, {
-        message: 'Hay campos que no se encuentran definidos en la tabla consultada'
-    })
-    relations: string[];
+  @IsOptional()
+  @ArrayNotEmpty({
+    message: `Existe 'relations' pero se encuetra vacio`,
+  })
+  @ArrayContainsSome(languageRelations, {
+    message: 'Hay campos que no se encuentran definidos en la tabla consultada',
+  })
+  relations: string[];
 }
