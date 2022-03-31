ec24a99964cff8615082f2178eb48247fd4a4bf7
Luco97 <renatoluco@gmail.com>
2022-03-31 20:52:28 -0300

    [ADD] se agrega nueva entidad en llamada a ORM typeORM

--- src/db/manga-entities/manga-entities.module.ts
+++ src/db/manga-entities/manga-entities.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import {
   ArtistEntity,
+  FavoriteEntity,
   GenreEntity,
   LanguageEntity,
   MangaEntity,
@@ -20,6 +21,7 @@ import {
       ArtistEntity,
       LanguageEntity,
       GenreEntity,
+      FavoriteEntity
     ]),
   ],
   exports: [MangaEntityService, ArtistEntityService, LanguageEntityService, GenreEntityService],
