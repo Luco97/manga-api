44d13dc355cd690aa7d112698828a2164702e339
Luco97 <renatoluco@gmail.com>
2022-04-01 19:31:13 -0300

    [CHG] se formatea entidad de idiomas

--- src/db/manga-entities/entity/language.entity.ts
+++ src/db/manga-entities/entity/language.entity.ts
@@ -1,71 +1,75 @@
-import { Exclude } from "class-transformer";
-import { Column, CreateDateColumn, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Exclude } from 'class-transformer';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { MangaEntity } from './manga.entity';
 
 @Entity({
-    name: 'IDIOMA'
+  name: 'IDIOMA',
 })
 export class LanguageEntity {
+  constructor(language: string, country: string) {
+    this.language = language;
+    this.country = country;
+  }
 
-    constructor( language: string, country: string) {
-        this.language   = language;
-        this.country    = country;
-    }
-    
-    @PrimaryGeneratedColumn({
-        name: 'ID',
-        type: 'int'
-    })
-    id: number;
+  @PrimaryGeneratedColumn({
+    name: 'ID',
+    type: 'int',
+  })
+  id: number;
 
-    @Column({
-        name: 'LANGUAGE',
-        type: 'varchar',
-        length: 30,
-        nullable: false
-    })
-    language: string;
+  @Column({
+    name: 'LANGUAGE',
+    type: 'varchar',
+    length: 30,
+    nullable: false,
+  })
+  language: string;
 
-    @Column({
-        name: 'COUNTRY',
-        type: 'varchar',
-        length: 30,
-        nullable: false
-    })
-    country: string;
+  @Column({
+    name: 'COUNTRY',
+    type: 'varchar',
+    length: 30,
+    nullable: false,
+  })
+  country: string;
 
-    @CreateDateColumn({
-        name: 'CREATED_AT',
-        type: 'timestamp',
-        select: false
-    })
-    @Exclude()
-    createDate: Date;
-    
-    @UpdateDateColumn({
-        name: 'UPDATED_AT',
-        type: 'timestamp',
-        select: false
-    })
-    @Exclude()
-    updateDate: Date;
+  @CreateDateColumn({
+    name: 'CREATED_AT',
+    type: 'timestamp',
+    select: false,
+  })
+  @Exclude()
+  createDate: Date;
 
-    @ManyToMany(
-        () => MangaEntity, manga => manga.languages,
-        {
-            nullable: true
-        }
-    )
-    @JoinTable({
-        name: 'TRANSLATE',
-        joinColumn: {
-            name: 'LANGUAGE_ID',
-            referencedColumnName: 'id'
-        },
-        inverseJoinColumn: {
-            name: 'MANGA_ID',
-            referencedColumnName: 'id'
-        }
-    })
-    mangas: MangaEntity[];
+  @UpdateDateColumn({
+    name: 'UPDATED_AT',
+    type: 'timestamp',
+    select: false,
+  })
+  @Exclude()
+  updateDate: Date;
+
+  @ManyToMany(() => MangaEntity, (manga) => manga.languages, {
+    nullable: true,
+  })
+  @JoinTable({
+    name: 'TRANSLATE',
+    joinColumn: {
+      name: 'LANGUAGE_ID',
+      referencedColumnName: 'id',
+    },
+    inverseJoinColumn: {
+      name: 'MANGA_ID',
+      referencedColumnName: 'id',
+    },
+  })
+  mangas: MangaEntity[];
 }
