5259906366ffe0ede127c3d6b3b77e3b504e1603
Luco97 <renatoluco@gmail.com>
2022-04-01 19:32:35 -0300

    [CHG] se forkmatea entidad de usuariso y se quitan exportaciones sin utilizar

--- src/db/user-entity/entity/user.entity.ts
+++ src/db/user-entity/entity/user.entity.ts
@@ -1,85 +1,88 @@
-import { MangaEntity } from "@db/manga/entity";
-import { genSalt, hash } from "bcrypt";
-import { Exclude } from "class-transformer";
-import { BeforeInsert, BeforeUpdate, Column, CreateDateColumn, Entity, ManyToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-
+import { MangaEntity } from '@db/manga/entity';
+import { genSalt, hash } from 'bcrypt';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity({
-    name: 'USUARIOS'
+  name: 'USUARIOS',
 })
 export class UserEntity {
+  constructor(username: string, email: string, password: string) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
 
-    constructor( username: string, email: string, password: string) {
-        this.username   = username;
-        this.email      = email;
-        this.password   = password;
-    }
+  @PrimaryGeneratedColumn({
+    name: 'id',
+  })
+  id: number;
 
-    @PrimaryGeneratedColumn({
-        name: 'id'
-    })
-    id: number;
+  @Column({
+    name: 'USERNAME',
+    type: 'varchar',
+    length: 30,
+    nullable: false,
+  })
+  username: string;
 
-    @Column({
-        name: 'USERNAME',
-        type: 'varchar',
-        length: 30,
-        nullable: false
-    })
-    username: string;
+  @Column({
+    name: 'EMAIL',
+    type: 'varchar',
+    length: 50,
+    nullable: false,
+  })
+  email: string;
 
-    @Column({
-        name: 'EMAIL',
-        type: 'varchar',
-        length: 50,
-        nullable: false
-    })
-    email: string;
+  @Column({
+    name: 'PASSWORD',
+    type: 'varchar',
+    nullable: false,
+    select: false,
+  })
+  password: string;
 
-    @Column({
-        name: 'PASSWORD',
-        type: 'varchar',
-        nullable: false,
-        select: false
-    })
-    password: string;
+  @Column({
+    name: 'ACTIVE',
+    type: 'boolean',
+    nullable: true,
+  })
+  active: boolean;
 
-    @Column({
-        name: 'ACTIVE',
-        type: 'boolean',
-        nullable: true
-    })
-    active: boolean;
+  @CreateDateColumn({
+    name: 'CREATED_AT',
+    type: 'timestamp',
+    select: false,
+  })
+  createDate: Date;
 
-    @CreateDateColumn({
-        name: 'CREATED_AT',
-        type: 'timestamp',
-        select: false
-    })
-    createDate: Date;
+  @UpdateDateColumn({
+    name: 'UPDATED_AT',
+    type: 'timestamp',
+    select: false,
+  })
+  updateDate: Date;
 
-    @UpdateDateColumn({
-        name: 'UPDATED_AT',
-        type: 'timestamp',
-        select: false
-    })
-    updateDate: Date;
-    
-    @ManyToMany(
-        () => MangaEntity, (manga) => manga.users,
-        {
-        nullable: true
-    })
-    mangas: MangaEntity[];
+  @ManyToMany(() => MangaEntity, (manga) => manga.users, {
+    nullable: true,
+  })
+  mangas: MangaEntity[];
 
-    @BeforeInsert()
-    @BeforeUpdate()
-    async hashPassword() {
-        if(!this.password) return;
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword() {
+    if (!this.password) return;
 
-        const saltRound: number = 10;
-        const bcSaltRound = await genSalt(saltRound);
-        this.password = await hash(this.password, bcSaltRound);
-    }
-
+    const saltRound: number = 10;
+    const bcSaltRound = await genSalt(saltRound);
+    this.password = await hash(this.password, bcSaltRound);
+  }
 }
