99beb12cee8866096a3567c59524653a9f1cb6ca
Luco97 <renatoluco@gmail.com>
2022-04-01 19:30:51 -0300

    [CHG] se formatea entidad de generos

--- src/db/manga-entities/entity/genre.entity.ts
+++ src/db/manga-entities/entity/genre.entity.ts
@@ -1,61 +1,65 @@
-import { Exclude } from "class-transformer";
-import { Column, CreateDateColumn, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Exclude } from 'class-transformer';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { MangaEntity } from './manga.entity';
 
 @Entity({
-    name: 'GENERO'
+  name: 'GENERO',
 })
 export class GenreEntity {
+  constructor(tag: string) {
+    this.tag = tag;
+  }
 
-    constructor( tag: string) {
-        this.tag = tag;
-    }
+  @PrimaryGeneratedColumn({
+    name: 'ID',
+  })
+  id: number;
 
-    @PrimaryGeneratedColumn({
-        name: 'ID'
-    })
-    id: number;
+  @Column({
+    name: 'TAG',
+    type: 'varchar',
+    length: 30,
+    nullable: false,
+  })
+  tag: string;
 
-    @Column({
-        name: 'TAG',
-        type: 'varchar',
-        length: 30,
-        nullable: false
-    })
-    tag: string;
+  @CreateDateColumn({
+    name: 'CREATED_AT',
+    type: 'timestamp',
+    select: false,
+  })
+  @Exclude()
+  createDate: Date;
 
-    @CreateDateColumn({
-        name: 'CREATED_AT',
-        type: 'timestamp',
-        select: false
-    })
-    @Exclude()
-    createDate: Date;
+  @UpdateDateColumn({
+    name: 'UPDATED_AT',
+    type: 'timestamp',
+    select: false,
+  })
+  @Exclude()
+  updateDate: Date;
 
-    @UpdateDateColumn({
-        name: 'UPDATED_AT',
-        type: 'timestamp',
-        select: false
-    })
-    @Exclude()
-    updateDate: Date;
-
-    @ManyToMany(
-        () => MangaEntity, manga => manga.genres,
-        {
-            nullable: true
-        }
-    )
-    @JoinTable({
-        name: 'CATEGORY',
-        joinColumn: {
-            name: 'GENRE_ID',
-            referencedColumnName: 'id'
-        },
-        inverseJoinColumn: {
-            name: 'MANGA_ID',
-            referencedColumnName: 'id'
-        }
-    })
-    mangas: MangaEntity[];
+  @ManyToMany(() => MangaEntity, (manga) => manga.genres, {
+    nullable: true,
+  })
+  @JoinTable({
+    name: 'CATEGORY',
+    joinColumn: {
+      name: 'GENRE_ID',
+      referencedColumnName: 'id',
+    },
+    inverseJoinColumn: {
+      name: 'MANGA_ID',
+      referencedColumnName: 'id',
+    },
+  })
+  mangas: MangaEntity[];
 }
