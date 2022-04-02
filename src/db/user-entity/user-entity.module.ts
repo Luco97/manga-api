19a20b8acf39ed8b760661169827f013c9a1b4af
Luco97 <renatoluco@gmail.com>
2022-04-02 15:54:36 -0300

    [CHG] se formatea modulo de entidad usuarios

--- src/db/user-entity/user-entity.module.ts
+++ src/db/user-entity/user-entity.module.ts
@@ -3,13 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entity';
 import { UserEntityService } from './service';
 
-
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([UserEntity])
-  ],
-  exports: [UserEntityService]
-  ,
-  providers: [UserEntityService]
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  exports: [UserEntityService],
+  providers: [UserEntityService],
 })
 export class UserEntityModule {}
