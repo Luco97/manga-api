efec83d60e1e56bb2b0b41514806bfa1df72690a
Luco97 <renatoluco@gmail.com>
2022-04-03 01:07:42 -0400

    [CHG] se fomratea servicio para JWT

--- src/shared/services/jwt.service.ts
+++ src/shared/services/jwt.service.ts
@@ -3,31 +3,32 @@ import { JwtService as JWTservice } from '@nestjs/jwt';
 
 @Injectable()
 export class JwtService {
+  constructor(private JWT: JWTservice) {}
 
-    constructor( private JWT: JWTservice) {}
-
-    generateJWT( user_id: number, username: string): string {
-        const value = this.JWT.sign({
-            id: user_id,
-            username
-        })
-        return value;
+  generateJWT(user_id: number, username: string): string {
+    const value = this.JWT.sign({
+      id: user_id,
+      username,
+    });
+    return value;
+  }
+  validateJWT(token: string): boolean {
+    try {
+      const payload = this.JWT.verify(token, { secret: process.env.JWT_SEED });
+      return payload.id && payload.username;
+    } catch (error) {
+      return false;
     }
-    validateJWT( token: string ): boolean {
-        try {
-            const payload = this.JWT.verify( token, {secret: process.env.JWT_SEED})
-            return (payload.id && payload.username);
-        } catch (error) {
-            return false;
-        }
+  }
+  getObjectJWT(token: string): { id?: number; username?: string } {
+    if (this.validateJWT(token)) {
+      const payload: { id: number; username: string } = this.JWT.verify(token, {
+        secret: process.env.JWT_SEED,
+      });
+      return {
+        id: payload.id,
+        username: payload.username,
+      };
     }
-    getObjectJWT( token: string): { id?: number, username?: string} {
-        if(this.validateJWT(token)) {
-            const payload: { id: number, username: string} = this.JWT.verify( token, {secret: process.env.JWT_SEED});
-            return {
-                id: payload.id,
-                username: payload.username
-            };
-        }
-    }
+  }
 }
