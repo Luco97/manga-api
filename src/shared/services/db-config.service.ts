a7004e7ff25da421d2d27f62ad6f60899ab4dc30
Luco97 <renatoluco@gmail.com>
2022-04-20 17:38:45 -0400

    [ADD] se agregan configuraciones para prod y dev

--- src/shared/services/db-config.service.ts
+++ src/shared/services/db-config.service.ts
@@ -1,31 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-
+import { LoggerOptions } from 'typeorm';
 
 @Injectable()
 export class DbConfigService {
-  getTypeORMconfig(entity: string = '/../**/*.entity.ts'): TypeOrmModuleOptions {
+  getTypeORMconfig(
+    entity: string = '/../**/*.entity.ts',
+  ): TypeOrmModuleOptions {
     return {
-       type: 'postgres',
-      
-      // host: process.env.POSTGRES_HOST,
-      // port: parseInt(process.env.POSTGRES_PORT),
-      // username: process.env.POSTGRES_USER,
-      // password: process.env.POSTGRES_PASSWORD,
-      // database: process.env.POSTGRES_DATABASE, 
-      url: process.env.DATABASE_URL,
-      entities: [
-        __dirname + entity
-      ],
-      //synchronize: true,
+      type: 'postgres',
+
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      // url: process.env.DATABASE_URL,
+      entities: [__dirname + entity],
       autoLoadEntities: true,
-      //logging: ['query'],
-      ssl: true,
-      extra: {
-        ssl: {
-          rejectUnauthorized: false
-        }
-      }
+      logging: process.env.LOG.split('-') as LoggerOptions,
+      ...this.enviromentConfig(),
     };
+  }
+
+  private enviromentConfig() {
+    switch (process.env.ENVIRONMENT) {
+      case 'development':
+        return {
+          synchronize: true,
+        };
+
+      case 'production':
+        return {
+          synchronize: false,
+          ssl: true,
+          extra: {
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          },
+        };
+    }
   }
 }
