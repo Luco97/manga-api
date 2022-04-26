e687d59d4e9420f1b47703fa65770ddd77b48123
Luco97 <renatoluco@gmail.com>
2022-04-26 19:12:40 -0400

    [ADD] se agrega objeto con columnas para buscar por relaciones de un manga

--- src/db/manga-entities/const/relations.ts
+++ src/db/manga-entities/const/relations.ts
@@ -6,6 +6,12 @@ export const languageRelations: string[] = ['mangas'];
 
 export const mangaRelations: string[] = ['genres', 'languages', 'artists'];
 
+export const searchByColumn = {
+  artists: 'name',
+  genres: 'tag',
+  languages: 'language',
+};
+
 export const mangaColumns: string[] = [
   'id',
   'title',
