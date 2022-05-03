355f8cf7b175250b70d7f662f1439409accd185c
Luco97 <renatoluco@gmail.com>
2022-05-02 23:26:26 -0400

    [ADD] se crea nueva peticion para saber si un manga en especifico es favorito en caso de llegar con token, sino es siempre falso

--- src/manga/controller/user.controller.ts
+++ src/manga/controller/user.controller.ts
@@ -15,12 +15,10 @@ import { UserService } from '@manga/services';
 import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('user')
-@UseGuards(AuthGuard)
 export class UserController {
-  constructor(
-    private _userService: UserService /* UserEntityService */,
-  ) {}
+  constructor(private _userService: UserService /* UserEntityService */) {}
 
+  @UseGuards(AuthGuard)
   @Post('favorites')
   async getFavorites(
     @Body() getBody: getFavorite,
@@ -44,6 +42,7 @@ export class UserController {
     }
   }
 
+  @UseGuards(AuthGuard)
   @Put('favorites')
   async favoriteManga(
     //@Param('id', ParseIntPipe) id: number,
@@ -55,6 +54,35 @@ export class UserController {
       const { id } = newFavorite.user;
       const foo: { response: response; data?: User } =
         await this._userService.setFavorite(id, newFavorite);
+      return res.status(foo.response.status).json(foo);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        response: {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Error en el servidor',
+        },
+      });
+    }
+  }
+
+  @Put('check')
+  async isFavorite(
+    @Body() Favorite: setFavorite,
+    @Res() res: Response<{ response: response; isFavorite?: boolean }>,
+  ) {
+    try {
+      // console.log('Controlador');
+      const { id } = Favorite.user;
+      if (!id)
+        return res.json({
+          response: {
+            status: HttpStatus.OK,
+            message: 'Usuario no autenticado, no es favorito ni en pedo',
+          },
+          isFavorite: false,
+        });
+      const foo: { response: response; isFavorite?: boolean } =
+        await this._userService.checkFavorite(Favorite.manga.id, id);
       return res.status(foo.response.status).json(foo);
     } catch (error) {
       return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
