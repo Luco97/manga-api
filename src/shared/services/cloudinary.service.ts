eecb986de486a3630688fd815f08953c53fbf287
Luco97 <renatoluco@gmail.com>
2022-05-07 23:57:28 -0400

    [ADD] se crea servicio para subir imagen a cloudinary

---
+++ src/shared/services/cloudinary.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { v2 } from 'cloudinary';
+
+@Injectable()
+export class CloudinaryService {
+  private _cloudinary = v2;
+
+  constructor() {
+    this._cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+      secure: true,
+    });
+  }
+
+  async upload(
+    filePath: string,
+    folderParth: string,
+  ): Promise<{ result?: any; error?: any }> {
+    const post = await this._cloudinary.uploader.upload(filePath, {
+      folder: folderParth,
+    });
+    if (post?.error) return { error: post.error };
+    return { result: post };
+  }
+}
