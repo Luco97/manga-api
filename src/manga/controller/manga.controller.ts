bd066307e17209b84954d82b1321405502ed55bc
Luco97 <renatoluco@gmail.com>
2022-05-08 19:57:39 -0400

    [ADD] se crea endpoint para subir imagenes desde el back a cloudinary

--- src/manga/controller/manga.controller.ts
+++ src/manga/controller/manga.controller.ts
@@ -10,16 +10,25 @@ import {
   Delete,
   Put,
   Query,
+  UseInterceptors,
+  UploadedFiles,
 } from '@nestjs/common';
 import { Response } from 'express';
-import { Manga, response } from '@interface/mangaResponses.interface';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 import { createMangaDto, readMangaDto, updateMangaDto } from '@manga/dto';
 import { MangaService } from '@manga/services';
 import { AuthGuard } from '../guards/auth.guard';
+import { Manga, response } from '@interface/mangaResponses.interface';
+import { CloudinaryService } from '@shared/services';
+import { unlink } from 'fs';
 
 @Controller('manga')
 export class MangaController {
-  constructor(private _mangaService: MangaService) {}
+  constructor(
+    private _mangaService: MangaService,
+    private _cloudinaryService: CloudinaryService,
+  ) {}
 
   @Post()
   async allManga(
@@ -118,6 +127,62 @@ export class MangaController {
         response: {
           status: HttpStatus.INTERNAL_SERVER_ERROR,
           message: 'Error en el servidor',
+        },
+      });
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('manga_pages/:manga_name')
+  @UseInterceptors(
+    FilesInterceptor('files', 3, {
+      storage: diskStorage({
+        destination: './archives/mangas',
+        filename: (req, file, cb) => cb(null, file.originalname),
+      }),
+    }),
+  )
+  async uploadImages(
+    @Param('manga_name') manga_name: string,
+    @UploadedFiles() files: Express.Multer.File[],
+    @Res()
+    res: Response<{
+      response: response;
+      data?: { result?: any; error?: any }[];
+    }>,
+  ) {
+    try {
+      const promiseArray: Promise<{ result?: any; error?: any }>[] = [];
+      files.forEach((element, index) => {
+        promiseArray.push(
+          this._cloudinaryService.upload(
+            element.path,
+            `mangas/${manga_name}`,
+            `${index + 1}`,
+          ),
+        );
+      });
+      const resolveArray: { result?: any; error?: any }[] = await Promise.all(
+        promiseArray,
+      );
+      if (resolveArray.length) {
+        files.forEach((element) => {
+          unlink(element.path, (error) => {});
+        });
+        return res.json({
+          response: {
+            status: HttpStatus.CREATED,
+            message: 'Imagenes en cloudinary',
+          },
+          data: resolveArray,
+        });
+      }
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        response: {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: 'Error en el servidor',
+          error,
         },
       });
     }
