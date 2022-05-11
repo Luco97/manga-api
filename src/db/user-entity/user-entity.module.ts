56245fcdeac8645d3a2dadb882acc4f17afc772b
Luco97 <renatoluco@gmail.com>
2022-05-10 20:26:01 -0400

    [ADD] se agregan elementos asociados a la entidad de roles

--- src/db/user-entity/user-entity.module.ts
+++ src/db/user-entity/user-entity.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from './entity';
-import { UserEntityService } from './service';
+import { RoleEntity, UserEntity } from './entity';
+import { RoleEntityService, UserEntityService } from './service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, RoleEntity])],
   exports: [UserEntityService],
-  providers: [UserEntityService],
+  providers: [UserEntityService, RoleEntityService],
 })
 export class UserEntityModule {}
