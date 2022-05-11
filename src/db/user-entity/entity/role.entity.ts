3d48653c7508a126935a192dd50c21b441614e33
Luco97 <renatoluco@gmail.com>
2022-05-10 20:22:44 -0400

    [ADD] se crea entidad para roles

---
+++ src/db/user-entity/entity/role.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { UserEntity } from './user.entity';
+
+@Entity({
+  name: 'ROLES',
+})
+export class RoleEntity {
+  @PrimaryGeneratedColumn('uuid', {
+    name: 'ID_ROLE',
+    comment: 'ID de un rol, que este comentario exista en algun lado',
+  })
+  uuid: string;
+
+  @Column({
+    name: 'ROLE_NAME',
+    nullable: false,
+    select: false,
+  })
+  role_name: string;
+
+  @OneToMany(() => UserEntity, (user) => user.role)
+  users: UserEntity[];
+}
