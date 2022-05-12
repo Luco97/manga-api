7647b9e279eafd562853578374f239fc94c21037
Luco97 <renatoluco@gmail.com>
2022-05-12 00:21:33 -0400

    [ADD] se crean metodos para obtener rol de un usuario

--- src/db/user-entity/service/role-entity.service.ts
+++ src/db/user-entity/service/role-entity.service.ts
@@ -10,4 +10,21 @@ export class RoleEntityService {
     @InjectRepository(RoleEntity)
     private _roleRepository: Repository<RoleEntity>,
   ) {}
+
+  async find(id: number): Promise<number> {
+    return this._roleRepository
+      .createQueryBuilder('role')
+      .innerJoin('role.users', 'user', 'user.id = :id', {id})
+      .where('role.role_name = :type', {type: 'admin'})
+      .orderBy()
+      .getCount();
+  }
+
+  get getCommon(): Promise<RoleEntity> {
+    return this._roleRepository.findOne({
+      where: {
+        role_name: 'common'
+      }
+    })
+  }
 }
