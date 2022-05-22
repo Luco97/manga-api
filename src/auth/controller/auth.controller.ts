cfacb14ab0f595134c6847149cf15878cff39a35
Luco97 <renatoluco@gmail.com>
2022-05-22 02:43:03 -0400

    [ADD] se agrega peticion para confirmar email existentes

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post, Res, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Res,
+  HttpStatus,
+  Get,
+  Param,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { createUserDto, loginUserDto } from '@auth/dto';
 import { AuthService } from 'auth/services/auth.service';
@@ -6,44 +14,47 @@ import { response, userResponse } from '@interface/authResponses.interface';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private _authService: AuthService) {}
 
-    constructor(
-        private _authService: AuthService
-    ) {}
-
-    @Post('sign')
-    async sign(
-        @Body() createUser: createUserDto,
-        @Res() res: Response<response>
-    ) {
-        try {
-            const foo: response = await this._authService.signUser(createUser);
-            return res.status(foo.status).json(foo);
-
-        } catch (error) {
-            return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .json({
-                            status: HttpStatus.INTERNAL_SERVER_ERROR,
-                            message: 'Error en el servidor'
-                        })
-        }
+  @Post('sign')
+  async sign(
+    @Body() createUser: createUserDto,
+    @Res() res: Response<response>,
+  ) {
+    try {
+      const foo: response = await this._authService.signUser(createUser);
+      return res.status(foo.status).json(foo);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: 'Error en el servidor',
+      });
     }
+  }
 
-    @Post('login')
-    async login( 
-        @Body() login: loginUserDto,
-        @Res() res: Response<userResponse>
-    ) {
-        try {
-            const foo: response = await this._authService.loginUser(login);
-            return res.status(foo.status).json(foo);
-        } catch (error) {
-            return res.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .json({
-                            status: HttpStatus.INTERNAL_SERVER_ERROR,
-                            message: 'Error en el servidor'
-                        })
-        }
+  @Post('login')
+  async login(@Body() login: loginUserDto, @Res() res: Response<userResponse>) {
+    try {
+      const foo: response = await this._authService.loginUser(login);
+      return res.status(foo.status).json(foo);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: 'Error en el servidor',
+      });
     }
+  }
 
+  @Get('email-verify/:guid')
+  async confirm(@Param('guid') id: string, @Res() res: Response<response>) {
+    try {
+      const foo: response = await this._authService.emailConfirm(id);
+      return res.status(foo.status).json(foo);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: 'Error en el servidor',
+      });
+    }
+  }
 }
