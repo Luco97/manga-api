60537d21bb3433805a88717ea256b7c92c6f3458
Luco97 <renatoluco@gmail.com>
2022-05-27 20:47:28 -0400

    [CHG] se cambia logica, solo clientes escuchan mangas puntuales, mangas creados son entregados a todo en el servidor

--- src/manga/events/events.gateway.ts
+++ src/manga/events/events.gateway.ts
@@ -11,14 +11,8 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayInit {
   constructor(private _utilsService: UtilsService) {}
 
   afterInit(server: Server) {
-    this._utilsService.mangaFavoriteSubject.subscribe(({ response, data }) => {
-      server.emit(`like/manga/${data.id}`, {
-        response,
-        data,
-      });
-    });
-    this._utilsService.mangaDropSubject.subscribe(({ response, data }) => {
-      server.emit(`dislike/manga/${data.id}`, {
+    this._utilsService.mangaCreateSubject.subscribe(({ response, data }) => {
+      server.emit('create/manga', {
         response,
         data,
       });
@@ -28,8 +22,14 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayInit {
   handleConnection(client: Socket, ...args: any[]) {
     //listener que emite los mangas que fueron creados
     //desde que alguien a iniciado una conexion con el servidor
-    this._utilsService.mangaCreateSubject.subscribe(({ response, data }) => {
-      client.emit('create/manga', {
+    this._utilsService.mangaFavoriteSubject.subscribe(({ response, data }) => {
+      client.emit(`like/manga/${data.id}`, {
+        response,
+        data,
+      });
+    });
+    this._utilsService.mangaDropSubject.subscribe(({ response, data }) => {
+      client.emit(`dislike/manga/${data.id}`, {
         response,
         data,
       });
