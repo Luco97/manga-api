bd8b6d3231b67db9987d23e01704e81c5b5b599d
Luco97 <renatoluco@gmail.com>
2022-05-29 03:17:00 -0400

    [ADD] se exporta servicio en index

--- src/manga/services/index.ts
+++ src/manga/services/index.ts
@@ -3,4 +3,5 @@ export * from './genre.service';
 export * from './language.service';
 export * from './manga.service';
 export * from './user.service';
+export * from './comment.service';
 export * from './utils.service';
