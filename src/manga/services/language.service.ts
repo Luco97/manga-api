fb0ee88a1e932eb375e870bbdd04b1ad3d77071a
Luco97 <renatoluco@gmail.com>
2022-05-31 00:25:10 -0400

    [CHG] se adapta codigo para patron de observables

--- src/manga/services/language.service.ts
+++ src/manga/services/language.service.ts
@@ -3,112 +3,156 @@ import { LanguageEntityService } from '@db/manga/services';
 import { Language, response } from '@interface/mangaResponses.interface';
 import { createLanguageDto } from '@manga/dto';
 import { Injectable, HttpStatus } from '@nestjs/common';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class LanguageService {
   constructor(private _languageService: LanguageEntityService) {}
 
-  async getAll(): Promise<{ response: response; data: Language[] }> {
-    const data: LanguageEntity[] = await this._languageService.findAll([]);
-    if (data.length) {
-      return {
-        response: {
-          status: HttpStatus.OK,
-          message: 'getAll idiomas',
-        },
-        data,
-      };
-    }
-    return {
-      response: {
-        status: HttpStatus.OK,
-        message: 'Sin idiomas',
+  getAll(): Observable<{ response: response; data: Language[] }> {
+    const observer = new Observable<{ response: response; data: Language[] }>(
+      (subscriber) => {
+        this._languageService
+          .findAll([])
+          .subscribe((data: LanguageEntity[]) => {
+            if (data.length) {
+              subscriber.next({
+                response: {
+                  status: HttpStatus.OK,
+                  message: 'getAll idiomas',
+                },
+                data,
+              });
+            } else {
+              subscriber.next({
+                response: {
+                  status: HttpStatus.OK,
+                  message: 'Sin idiomas',
+                },
+                data: [],
+              });
+            }
+            subscriber.complete();
+          });
       },
-      data: [],
-    };
+    );
+    return observer;
   }
 
-  async getOne(
+  getOne(
     id: number,
     relations: string[],
-  ): Promise<{ response: response; data?: Language }> {
-    const data: LanguageEntity[] = await this._languageService.findBy({
-      relations,
-      where: {
-        id,
+  ): Observable<{ response: response; data?: Language }> {
+    const observer = new Observable<{ response: response; data?: Language }>(
+      (subscriber) => {
+        this._languageService
+          .findBy({
+            relations,
+            where: {
+              id,
+            },
+          })
+          .subscribe((data: LanguageEntity[]) => {
+            if (data.length) {
+              subscriber.next({
+                response: {
+                  status: HttpStatus.OK,
+                  message: 'getOne idioma',
+                },
+                data: data.pop(),
+              });
+            } else {
+              subscriber.next({
+                response: {
+                  status: HttpStatus.NOT_FOUND,
+                  message: 'No encontrado',
+                },
+              });
+            }
+            subscriber.complete();
+          });
       },
-    });
-    if (data.length) {
-      return {
-        response: {
-          status: HttpStatus.OK,
-          message: 'getOne idioma',
-        },
-        data: data.pop(),
-      };
-    }
-    return {
-      response: {
-        status: HttpStatus.NOT_FOUND,
-        message: 'No encontrado',
-      },
-    };
-  }
-
-  async create(
-    createLanguage: createLanguageDto,
-  ): Promise<{ response: response; data?: Language }> {
-    const languages = await this._languageService.findBy({
-      where: {
-        language: createLanguage.language.toLocaleLowerCase(),
-      },
-    });
-    if (!languages.length) {
-      const newLanguage: LanguageEntity = new LanguageEntity(
-        createLanguage.language.toLocaleLowerCase(),
-        createLanguage.country.toLocaleLowerCase(),
-        createLanguage?.country_flag
-      );
-      const data = await this._languageService.create(newLanguage);
-      return {
-        response: {
-          status: HttpStatus.CREATED,
-          message: 'Idioma creado',
-        },
-        data: data,
-      };
-    }
-    return {
-      response: {
-        status: HttpStatus.CONFLICT,
-        message: 'Ya existe un idioma con ese nombre',
-      },
-      data: languages.pop(),
-    };
-  }
-
-  async delete(id: number): Promise<{ response: response; data?: Language }> {
-    const data: { response: response; data?: Language } = await this.getOne(
-      id,
-      [],
     );
-    if (data?.data) {
-      const language = await this._languageService.delete(
-        data.data as LanguageEntity,
-      );
-      return {
-        response: {
-          status: HttpStatus.OK,
-          message: 'Idioma borrado',
-        },
-        data: data.data,
-      };
-    }
-    return {
-      response: {
-        status: HttpStatus.NOT_FOUND,
-        message: `Idioma con id = '${id}' no existe`,
+    return observer;
+  }
+
+  create(
+    createLanguage: createLanguageDto,
+  ): Observable<{ response: response; data?: Language }> {
+    const observer = new Observable<{ response: response; data?: Language }>(
+      (subscriber) => {
+        this._languageService
+          .findBy({
+            where: {
+              language: createLanguage.language.toLocaleLowerCase(),
+            },
+          })
+          .subscribe((languages: LanguageEntity[]) => {
+            if (!languages.length) {
+              const newLanguage: LanguageEntity = new LanguageEntity(
+                createLanguage.language.toLocaleLowerCase(),
+                createLanguage.country.toLocaleLowerCase(),
+                createLanguage?.country_flag,
+              );
+              this._languageService
+                .create(newLanguage)
+                .subscribe((data: LanguageEntity) => {
+                  subscriber.next({
+                    response: {
+                      status: HttpStatus.CREATED,
+                      message: 'Idioma creado',
+                    },
+                    data: data,
+                  });
+                  subscriber.complete();
+                });
+            } else {
+              subscriber.next({
+                response: {
+                  status: HttpStatus.CONFLICT,
+                  message: 'Ya existe un idioma con ese nombre',
+                },
+                data: languages.pop(),
+              });
+              subscriber.complete();
+            }
+          });
       },
-    };
+    );
+    return observer;
+  }
+
+  delete(id: number): Observable<{ response: response; data?: Language }> {
+    const observer = new Observable<{ response: response; data?: Language }>(
+      (subscriber) => {
+        this.getOne(id, []).subscribe(
+          (data: { response: response; data?: Language }) => {
+            if (data?.data) {
+              this._languageService
+                .delete(data.data as LanguageEntity)
+                .subscribe((language: LanguageEntity) => {
+                  subscriber.next({
+                    response: {
+                      status: HttpStatus.OK,
+                      message: 'Idioma borrado',
+                    },
+                    data: data.data,
+                  });
+                  subscriber.complete();
+                });
+            } else {
+              subscriber.next({
+                response: {
+                  status: HttpStatus.NOT_FOUND,
+                  message: `Idioma con id = '${id}' no existe`,
+                },
+              });
+              subscriber.complete();
+            }
+          },
+        );
+      },
+    );
+    return observer;
   }
 }
