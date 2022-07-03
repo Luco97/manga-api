dd2605b7eeb3bc460f7816f8bbc9cb8cbe72fcfb
Luco97 <renatoluco@gmail.com>
2022-07-03 02:11:16 -0400

    [CHG] se modifica metodo para obtener un solo artista contand oel numero de mangas al que pertenece

--- src/db/manga-entities/services/artist.entity.service.ts
+++ src/db/manga-entities/services/artist.entity.service.ts
@@ -3,50 +3,67 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { FindManyOptions, Repository } from 'typeorm';
 import { ArtistEntity, MangaEntity } from '@db/manga/entity';
 
-
 @Injectable()
 export class ArtistEntityService {
-    constructor(
-        @InjectRepository(ArtistEntity)
-        private artistRepository: Repository<ArtistEntity>
-    ) {
-        
-       /*  this.findOne(4).then( (value) => {
+  constructor(
+    @InjectRepository(ArtistEntity)
+    private artistRepository: Repository<ArtistEntity>,
+  ) {
+    /*  this.findOne(4).then( (value) => {
             const a: ArtistEntity = value;
             this.delete(a);
         });
         this.findAll();
         this.findOne(6); */
-        /* this.findOne(3).then( (value) => {
+    /* this.findOne(3).then( (value) => {
             const a: ArtistEntity = value;
             this.delete(a);
         }); */
-    }
+  }
 
-    async findAll(relations: string[] = ['mangas']): Promise<ArtistEntity[]> {
-        const data = await this.artistRepository.find({relations});
-        return data;
-    }
+  async findAll(relations: string[] = ['mangas']): Promise<ArtistEntity[]> {
+    const data = await this.artistRepository.find({ relations });
+    return data;
+  }
 
-    async findOne(id: number, relations: string[] = ['mangas']): Promise<ArtistEntity> {
-        const data = await this.artistRepository.findOne(id, {relations});
-        if(!data) throw new NotFoundException(`No puede encontrar un artista con id=${id}`);
-        return data;
-    }
+  async findOne(
+    id: number,
+    relations: string[] = ['mangas'],
+  ): Promise<ArtistEntity> {
+    // const data = await this.artistRepository.findOne(id, {relations});
+    const data = await this.artistRepository
+      .createQueryBuilder('artist')
+      .loadRelationCountAndMap(
+        'artist.count',
+        'artist.mangas',
+        'asd',
+        (subQuery) => {
+          return subQuery.orderBy('cnt', 'ASC');
+        },
+      )
+      .where('artist.id = :id', { id })
+      .addOrderBy('artist.id')
+      .getOne();
+    if (!data)
+      throw new NotFoundException(`No puede encontrar un artista con id=${id}`);
+    return data;
+  }
 
-    async findBy(options: FindManyOptions<ArtistEntity>): Promise<ArtistEntity[]> {
-        const data = await this.artistRepository.find(options);
-        return data;
-    }
+  async findBy(
+    options: FindManyOptions<ArtistEntity>,
+  ): Promise<ArtistEntity[]> {
+    const data = await this.artistRepository.find(options);
+    return data;
+  }
 
-    async create( artist: ArtistEntity) {
-        /* const data = this.artistRepository.create({...artist} as ArtistEntity) */
-        const newArtist = this.artistRepository.create(artist);
-        return await this.artistRepository.save(newArtist);
-    }
+  async create(artist: ArtistEntity) {
+    /* const data = this.artistRepository.create({...artist} as ArtistEntity) */
+    const newArtist = this.artistRepository.create(artist);
+    return await this.artistRepository.save(newArtist);
+  }
 
-    async delete(artist: ArtistEntity) {
-        const data = await this.findOne(artist.id, []);
-        return await this.artistRepository.remove(data);
-    }
+  async delete(artist: ArtistEntity) {
+    const data = await this.findOne(artist.id, []);
+    return await this.artistRepository.remove(data);
+  }
 }
