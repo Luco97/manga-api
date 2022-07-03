706727111e9af5548fec972c469907de94b303c8
Luco97 <renatoluco@gmail.com>
2022-07-03 01:40:54 -0400

    [CHG] se puede contar usando un metodo de querybuilder, asi que se quita campo de likes

--- src/db/manga-entities/entity/manga.entity.ts
+++ src/db/manga-entities/entity/manga.entity.ts
@@ -55,15 +55,6 @@ export class MangaEntity {
   })
   pages: number;
 
-  @Column({
-    name: 'LIKES',
-    type: 'int',
-    update: true,
-    nullable: false,
-    default: 0,
-  })
-  likes: number;
-
   @CreateDateColumn({
     name: 'CREATED_AT',
     type: 'timestamp',
