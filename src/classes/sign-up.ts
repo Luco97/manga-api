ec57b94bcf5651b78d2cb1f7a1cdfefad0a6c1dc
Luco97 <renatoluco@gmail.com>
2022-08-01 23:34:56 -0400

    UPDATE se borran mensajes

--- src/classes/sign-up.ts
+++ src/classes/sign-up.ts
@@ -1,35 +1,22 @@
-import { IsEmail, IsNotEmpty, Length, ValidationArguments } from "class-validator";
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
 
 export class SignUp {
-    @IsNotEmpty({
-        message: 'email  no ingresado'
-    })
-    @IsEmail()
-    @Length(8,30, {
-        message: typeNumber(4, 30, 'email')
-    })
-    email;
+  @IsNotEmpty({
+    message: 'email  no ingresado',
+  })
+  @IsEmail()
+  @Length(8, 30)
+  email;
 
-    @IsNotEmpty({
-        message: 'username no ingresado'
-    })
-    @Length( 5, 18, {
-        message: typeNumber( 5, 18, 'username')
-    })
-    username;
+  @IsNotEmpty({
+    message: 'username no ingresado',
+  })
+  @Length(5, 18)
+  username;
 
-    @IsNotEmpty({
-        message: 'password no ingresada'
-    })
-    @Length( 6, 20, {
-        message: typeNumber( 6, 20, 'password')
-    })
-    password;
-}
-
-function typeNumber(min: number, max: number, name: string) {
-    return (validationArgument: ValidationArguments): string => {
-        if(validationArgument.value < min)  return `${name} bajo el minimo de caracteres`;
-        if(validationArgument.value > max) return  `${name} supera el maximo de caracteres`;
-    }
+  @IsNotEmpty({
+    message: 'password no ingresada',
+  })
+  @Length(6, 20)
+  password;
 }
